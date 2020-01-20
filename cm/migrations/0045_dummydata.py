b3e051ed5cc5e5cd8e3bc4952546506a0a3670b3
Alexandr Alferov <aas@arenadata.io>
2020-01-20 10:43:17 +0300

    ADCM-1081 Fixed linter error line-to-long.

--- cm/migrations/0045_dummydata.py
+++ cm/migrations/0045_dummydata.py
@@ -24,7 +24,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='DummyData',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
                 ('date', models.DateTimeField(auto_now=True)),
             ],
         ),
