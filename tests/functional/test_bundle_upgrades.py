871d661951de719be37aa61d42ef8b767243dd1a
Konstantin Voschanov <vka@arenadata.io>
2020-01-24 11:23:02 +0300

    ADCM-649 fix test

--- tests/functional/test_bundle_upgrades.py
+++ tests/functional/test_bundle_upgrades.py
@@ -142,4 +142,4 @@ def test_upgrade_contains_strict_and_nonstrict_value(client, boundary, expected)
     bundledir = os.path.join(BUNDLES, 'strict_and_non_strict_upgrade/' + boundary)
     with pytest.raises(coreapi.exceptions.ErrorMessage) as e:
         steps.upload_bundle(client, bundledir)
-    INVALID_VERSION_DEFINITION,.equal(e, expected, 'should be present')
+    INVALID_VERSION_DEFINITION.equal(e, expected, 'should be present')
