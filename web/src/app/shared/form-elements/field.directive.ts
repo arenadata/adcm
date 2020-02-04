1a2657dc3e74f13ebc22923d27d944c430d6d241
Davidov Pavel <dp@arenadata.io>
2020-02-04 16:11:10 +0300

    ADCM-1050 removed the attr 'disabled' from validation

--- web/src/app/shared/form-elements/field.directive.ts
+++ web/src/app/shared/form-elements/field.directive.ts
@@ -33,7 +33,7 @@ export class FieldDirective extends BaseDirective implements OnInit {
 
   get isValid() {
     const field = this.find();
-    return field.disabled || (field.valid && (field.dirty || field.touched));
+    return field.valid && (field.dirty || field.touched);
   }
 
   hasError(name: string) {
