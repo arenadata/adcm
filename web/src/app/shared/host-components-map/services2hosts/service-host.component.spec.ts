fdc6ef93b6b0bf209800e4709c328d4fccff507c
Davidov Pavel <dp@arenadata.io>
2020-06-05 09:55:41 +0300

    ADCM-1313 added new script on check relations and markers

--- web/src/app/shared/host-components-map/services2hosts/service-host.component.spec.ts
+++ web/src/app/shared/host-components-map/services2hosts/service-host.component.spec.ts
@@ -24,33 +24,34 @@ import { TakeService } from '../take.service';
 import { ServiceHostComponent } from './service-host.component';
 import { IRawHosComponent } from '../types';
 
+const hc = [
+  {
+    id: 1,
+    host_id: 2,
+    service_id: 2,
+    component_id: 4,
+  },
+  {
+    id: 2,
+    host_id: 2,
+    service_id: 2,
+    component_id: 2,
+  },
+  {
+    id: 3,
+    host_id: 2,
+    service_id: 2,
+    component_id: 3,
+  },
+  {
+    id: 4,
+    host_id: 2,
+    service_id: 1,
+    component_id: 1,
+  },
+];
 const raw: IRawHosComponent = {
-  hc: [
-    {
-      id: 1,
-      host_id: 2,
-      service_id: 2,
-      component_id: 4,
-    },
-    {
-      id: 2,
-      host_id: 2,
-      service_id: 2,
-      component_id: 2,
-    },
-    {
-      id: 3,
-      host_id: 2,
-      service_id: 2,
-      component_id: 3,
-    },
-    {
-      id: 4,
-      host_id: 2,
-      service_id: 1,
-      component_id: 1,
-    },
-  ],
+  hc: [],
   host: [
     {
       state: 'created',
@@ -123,6 +124,13 @@ describe('Service Host Map Component', () => {
 
   const initialState = { socket: {} };
 
+  const initDefault = (r: IRawHosComponent) => {
+    component.service.init(r);
+    component.hosts = component.service.Hosts;
+    component.serviceComponents = component.service.Components;
+    fixture.detectChanges();
+  };
+
   beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule],
@@ -143,10 +151,6 @@ describe('Service Host Map Component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ServiceHostComponent);
     component = fixture.componentInstance;
-    component.service.init(raw);
-    component.hosts = component.service.Hosts;
-    component.serviceComponents = component.service.Components;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -154,28 +158,97 @@ describe('Service Host Map Component', () => {
   });
 
   it('should display compnents and hosts as button', () => {
-    fixture.whenStable().then(() => {
-      const cElement: HTMLElement = fixture.nativeElement;
-      const components = cElement.querySelectorAll('.wrapper').item(0).querySelectorAll('app-much-2-many');
-      const hosts = cElement.querySelectorAll('.wrapper').item(1).querySelectorAll('app-much-2-many');
-      expect(hosts.length).toBe(1);
-      expect(components.length).toBe(4);
-      const host_relations = hosts.item(0).querySelector('.relations-list').children;
-      expect(host_relations.length).toBe(4);
-      components.forEach((a) => expect(a.querySelector('.relations-list').children.length).toBe(1));
-    });
+    raw.hc = hc;
+    initDefault(raw);
+    //fixture.whenStable().then(() => {
+    const cElement: HTMLElement = fixture.nativeElement;
+    const components = cElement.querySelectorAll('.wrapper').item(0).querySelectorAll('app-much-2-many');
+    const hosts = cElement.querySelectorAll('.wrapper').item(1).querySelectorAll('app-much-2-many');
+    expect(hosts.length).toBe(1);
+    expect(components.length).toBe(4);
+    const host_relations = hosts.item(0).querySelector('.relations-list').children;
+    expect(host_relations.length).toBe(4);
+    components.forEach((a) => expect(a.querySelector('.relations-list').children.length).toBe(1));
+    //});
   });
 
   it('should mark host/component as selected and(or) as linked', () => {
-    fixture.whenStable().then(() => {
-      const cElement: HTMLElement = fixture.nativeElement;
-      const components = cElement.querySelectorAll('.wrapper').item(0).querySelectorAll('app-much-2-many');
-      const hosts = cElement.querySelectorAll('.wrapper').item(1).querySelectorAll('app-much-2-many');
-      const host = hosts.item(0);
-      (host.querySelector('.m2m .title-container button.title') as HTMLElement).click();
-      fixture.detectChanges();
-      expect(host.querySelector('.m2m').classList.contains('selected')).toBeTrue();
-      components.forEach((a) => expect(a.querySelector('.m2m').classList.contains('linked')).toBeTrue());
-    });
+    raw.hc = hc;
+    initDefault(raw);
+    //fixture.whenStable().then(() => {
+    const cElement: HTMLElement = fixture.nativeElement;
+    const components = cElement.querySelectorAll('.wrapper').item(0).querySelectorAll('app-much-2-many');
+    const hosts = cElement.querySelectorAll('.wrapper').item(1).querySelectorAll('app-much-2-many');
+    const host = hosts.item(0);
+    (host.querySelector('.m2m .title-container button.title') as HTMLElement).click();
+    fixture.detectChanges();
+    expect(host.querySelector('.m2m').classList.contains('selected')).toBeTrue();
+    components.forEach((a) => expect(a.querySelector('.m2m').classList.contains('linked')).toBeTrue());
+    //});
+  });
+
+  it('should add relative on click and check linked and selected property', () => {
+    raw.hc = [];
+    initDefault(raw);
+    //fixture.whenStable().then(() => {
+    const cElement: HTMLElement = fixture.nativeElement;
+    const components = cElement.querySelectorAll('.wrapper').item(0).querySelectorAll('app-much-2-many');
+    const hosts = cElement.querySelectorAll('.wrapper').item(1).querySelectorAll('app-much-2-many');
+    const host = hosts.item(0);
+    const comp = components.item(0);
+    const host_btn = host.querySelector('.m2m .title-container button.title') as HTMLElement;
+    const comp_btn = comp.querySelector('.m2m .title-container button.title') as HTMLElement;
+    const host_relations = host.querySelector('.relations-list').children;
+    const comp_relations = comp.querySelector('.relations-list').children;
+
+    const comp_isLinked = () => comp.querySelector('.m2m').classList.contains('linked');
+    const comp_isSelect = () => comp.querySelector('.m2m').classList.contains('selected');
+    const host_isSelect = () => host.querySelector('.m2m').classList.contains('selected');
+    const host_isLinked = () => host.querySelector('.m2m').classList.contains('linked');
+
+    // start: host - select
+    host_btn.click();
+    fixture.detectChanges();
+    expect(host_isSelect()).toBeTrue();
+
+    comp_btn.click();
+    fixture.detectChanges();
+    expect(comp_isLinked()).toBeTrue();
+    expect(host_relations.length).toBe(1);
+    expect(comp_relations.length).toBe(1);
+
+    comp_btn.click();
+    fixture.detectChanges();
+    expect(comp_isLinked()).toBeFalse();
+    expect(host_relations.length).toBe(0);
+    expect(comp_relations.length).toBe(0);
+
+    host_btn.click();
+    fixture.detectChanges();
+    expect(host_isSelect()).toBeFalse();
+    // end
+
+    // start: component- select
+    comp_btn.click();
+    fixture.detectChanges();
+    expect(comp_isSelect()).toBeTrue();
+
+    host_btn.click();
+    fixture.detectChanges();
+    expect(host_isLinked()).toBeTrue();
+    expect(host_relations.length).toBe(1);
+    expect(comp_relations.length).toBe(1);
+
+    host_btn.click();
+    fixture.detectChanges();
+    expect(host_isLinked()).toBeFalse();
+    expect(host_relations.length).toBe(0);
+    expect(comp_relations.length).toBe(0);
+
+    comp_btn.click();
+    fixture.detectChanges();
+    expect(host_isSelect()).toBeFalse();
+    // end
+    //});
   });
 });
