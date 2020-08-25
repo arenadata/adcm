98e190e1f5f2cbdb05f2f8f23dbf403becd2afc5
Alexandr Alferov <aas@arenadata.io>
2020-08-25 13:35:46 +0300

    ADCM-1399 Fixed W0707 and R1725 for Pylint 2.6.0

--- python/api/cluster_serial.py
+++ python/api/cluster_serial.py
@@ -70,11 +70,11 @@ class ClusterSerializer(serializers.Serializer):
                 validated_data.get('description', ''),
             )
         except Prototype.DoesNotExist:
-            raise AdcmApiEx('PROTOTYPE_NOT_FOUND')
+            raise AdcmApiEx('PROTOTYPE_NOT_FOUND') from None
         except IntegrityError:
-            raise AdcmApiEx("CLUSTER_CONFLICT")
+            raise AdcmApiEx("CLUSTER_CONFLICT") from None
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code)
+            raise AdcmApiEx(e.code, e.msg, e.http_code) from e
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
@@ -83,7 +83,7 @@ class ClusterSerializer(serializers.Serializer):
             instance.save()
         except IntegrityError:
             msg = 'cluster with name "{}" already exists'.format(instance.name)
-            raise AdcmApiEx("CLUSTER_CONFLICT", msg)
+            raise AdcmApiEx("CLUSTER_CONFLICT", msg) from None
         return instance
 
 
@@ -196,7 +196,7 @@ class ClusterHostAddSerializer(ClusterHostDetailSerializer):
         try:
             cm.api.add_host_to_cluster(cluster, host)
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code)
+            raise AdcmApiEx(e.code, e.msg, e.http_code) from e
         return host
 
 
@@ -354,9 +354,9 @@ class ClusterServiceSerializer(serializers.Serializer):
                 validated_data.get('prototype_id'),
             )
         except IntegrityError:
-            raise AdcmApiEx('SERVICE_CONFLICT')
+            raise AdcmApiEx('SERVICE_CONFLICT') from None
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code)
+            raise AdcmApiEx(e.code, e.msg, e.http_code) from e
 
 
 class ClusterServiceDetailSerializer(ClusterServiceSerializer):
@@ -616,7 +616,7 @@ class DoBindSerializer(serializers.Serializer):
                 validated_data.get('export_service_id', 0)
             )
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code)
+            raise AdcmApiEx(e.code, e.msg, e.http_code) from e
 
 
 class DoServiceBindSerializer(serializers.Serializer):
@@ -639,7 +639,7 @@ class DoServiceBindSerializer(serializers.Serializer):
                 validated_data.get('export_service_id')
             )
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code)
+            raise AdcmApiEx(e.code, e.msg, e.http_code) from e
 
 
 class ClusterServiceConfigSerializer(serializers.Serializer):
@@ -722,7 +722,7 @@ class ObjectConfigUpdate(ObjectConfig):
             if hasattr(instance.obj_ref, 'adcm'):
                 logrotate.run()
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code, e.adds)
+            raise AdcmApiEx(e.code, e.msg, e.http_code, e.adds) from e
         return cl
 
 
@@ -735,7 +735,7 @@ class ObjectConfigRestore(ObjectConfig):
                 validated_data.get('description', instance.description)
             )
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code)
+            raise AdcmApiEx(e.code, e.msg, e.http_code) from e
         return cc
 
 
@@ -793,4 +793,4 @@ class PostImportSerializer(serializers.Serializer):
             service = self.context.get('service')
             return cm.api.multi_bind(cluster, service, bind)
         except AdcmEx as e:
-            raise AdcmApiEx(e.code, e.msg, e.http_code, e.adds)
+            raise AdcmApiEx(e.code, e.msg, e.http_code, e.adds) from e
