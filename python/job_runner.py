3aa60a17138a8743c2ad934284c46aa4f11e1ab0
Anton Chevychalov <cab@arenadata.io>
2021-02-03 17:11:18 +0300

    ADCM-1542 Force verbose to 4th level (vvvv)
    Initial commit was wrong and doesn't equal to original user story, which require "vvvv"

--- python/job_runner.py
+++ python/job_runner.py
@@ -110,7 +110,7 @@ def run_ansible(job_id):
         if 'ansible_tags' in conf['job']['params']:
             cmd.append('--tags=' + conf['job']['params']['ansible_tags'])
     if 'verbose' in conf['job'] and conf['job']['verbose']:
-        cmd.append('-vvv')
+        cmd.append('-vvvv')
 
     proc = subprocess.Popen(cmd, env=env_configuration(conf), stdout=out_file, stderr=err_file)
     log.info("job #%s run cmd: %s", job_id, ' '.join(cmd))
