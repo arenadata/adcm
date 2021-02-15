42d0c659e1975376a9ca108e0c88d5e07fc22569
Ann <dan@arenadata.io>
2021-02-15 14:43:23 +0300

    ADCM-1543 fix remarks

--- tests/ui_tests/test_actions_page.py
+++ tests/ui_tests/test_actions_page.py
@@ -46,9 +46,9 @@ def check_verbosity(log, verbose_state):
     assert ("verbosity: 4" in log.content) is verbose_state
 
 
-def test_check_verbose_checkbox_of_action_run_form_is_displayed(action_page):
+def test_check_verbose_checkbox_of_action_run_form_is_displayed(cluster_action_page):
     with allure.step("Check if verbose checkbox is displayed in popup from Action page"):
-        assert (action_page.check_verbose_chbx_displayed()), "Verbose checkbox is not displayed in the popup"
+        assert (cluster_action_page.check_verbose_chbx_displayed()), "Verbose checkbox is not displayed in the popup"
 
 
 @pytest.mark.parametrize("verbose_state", [True, False], ids=["verbose_state_true", "verbose_state_false"])
