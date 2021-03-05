932a98b43746dee0b2015aae4fb4cdbf3c05f020
Dmitry Chernukhin <madlabman@mail.ru>
2021-03-05 14:57:15 +0400

    Fix linters

--- tests/functional/test_related_sc.py
+++ tests/functional/test_related_sc.py
@@ -9,6 +9,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint: disable=too-many-locals
+
 import os
 from contextlib import contextmanager
 
