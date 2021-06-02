e9af1eb5b0b16fa3eb90f271de3e7bd7fc2e5b81
Alexandr Alferov <aas@arenadata.io>
2021-06-02 14:10:14 +0300

    Removed unused imports

--- python/cm/models.py
+++ python/cm/models.py
@@ -13,8 +13,6 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import User, Group, Permission
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
