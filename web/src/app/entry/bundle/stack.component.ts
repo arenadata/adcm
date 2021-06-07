00289d458b8411581c76ece1180f33606703cfb0
Aleksandr Lihih <sasha@awarenow.io>
2021-06-07 05:43:06 +0300

    ADCM-1741 Single bundle main view was redesigned

--- web/src/app/entry/bundle/stack.component.ts
+++ web/src/app/entry/bundle/stack.component.ts
@@ -9,7 +9,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { Component, ComponentRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ComponentRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -31,7 +31,8 @@ import { ApiService } from '@app/core/api';
   template: `
     <mat-toolbar class="toolbar">
       <app-crumbs [navigation]="[{ url: '/bundle', title: 'bundles' }]"></app-crumbs>
-      <app-button-uploader #uploadBtn [color]="'accent'" [label]="'Upload bundles'" (output)="upload($event)"></app-button-uploader>
+      <app-button-uploader #uploadBtn [color]="'accent'" [label]="'Upload bundles'"
+                           (output)="upload($event)"></app-button-uploader>
     </mat-toolbar>
 
     <adwp-list
@@ -100,23 +101,37 @@ export class StackComponent extends AdwpListDirective<IBundle> {
 @Component({
   selector: 'app-main',
   template: `
-    <table>
-      <tr *ngFor="let prop of keys(model)">
-        <td style="padding: 6px 20px;">{{ prop }}</td>
-        <td>{{ model[prop] }}</td>
-      </tr>
-    </table>
+    <adwp-list
+      [columns]="listColumns"
+      [dataSource]="toDataSource(model)"
+    ></adwp-list>
   `,
+  styles: [':host {width: 100%; max-width: 960px}'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent implements OnInit {
   model: any;
+
+  listColumns = [
+    ListFactory.keyColumn(),
+    ListFactory.valueColumn(),
+  ] as IColumns<IBundle>;
+
   constructor(private service: ClusterService) {}
 
   ngOnInit() {
     this.model = this.service.Current;
   }
 
-  keys(model: {}) {
-    return Object.keys(model);
+  toDataSource(model: {}): { results: any[]; count: number; } {
+    if (!model) {
+      return { results: [], count: 0 };
+    }
+
+    const results = Object.entries(model)
+      .reduce((acc, [key, value]) => [...acc, { key, value }], []);
+
+    return { results, count: 0 };
+
   }
 }
