8519e64af648318aea6f2e062bcc6cea0e4bbdd8
dgusakov <gdv@arenadata.io>
2021-07-02 09:33:34 +0300

    ADCM-1807 Add pre-filled ADCM to tests

---
+++ tests/functional/conftest.py
@@ -0,0 +1,15 @@
+"""Common fixtures for the functional tests"""
+import pytest
+import allure
+
+
+@allure.title("Additional ADCM init config")
+@pytest.fixture(scope="session", params=[{}, {"fill_dummy_data": True}], ids=["clean_adcm", "adcm_with_dummy_data"])
+def additional_adcm_init_config(request) -> dict:
+    """
+    Add options for ADCM init.
+    Redefine this fixture in the actual project to alter additional options of ADCM initialisation.
+    Ex. If this fixture will return {"fill_dummy_data": True}
+    then on the init stage dummy objects will be added to ADCM image
+    """
+    return request.param
