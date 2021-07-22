45739771572df20510bab56242e7b315725c495b
Nikolay Agarin <anv@arenadata.io>
2021-07-22 12:38:46 +0300

    add MessageTemplates and use it in AgendaItems

--- python/cm/unit_tests/utils.py
+++ python/cm/unit_tests/utils.py
@@ -140,11 +140,13 @@ def gen_action(name='', bundle=None, prototype=None) -> models.Action:
     )
 
 
-def gen_task_log(obj) -> models.TaskLog:
+def gen_task_log(obj: models.ADCMEntity) -> models.TaskLog:
+    selector = obj.get_id_chain()['ids']
     return models.TaskLog.objects.create(
         action=gen_action(),
         object_id=obj.pk,
         status='CREATED',
+        selector=selector,
         start_date=timezone.now(),
         finish_date=timezone.now(),
     )
