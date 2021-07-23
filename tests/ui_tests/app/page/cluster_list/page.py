4a63f2914b1497be0b3be953b15fbca7ddcb6b6d
Sealwing <egaraslanoff@yandex.ru>
2021-07-23 10:43:25 +0500

    Final sync with develop

--- tests/ui_tests/app/page/cluster_list/page.py
+++ tests/ui_tests/app/page/cluster_list/page.py
@@ -10,11 +10,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import allure
+
+from tests.ui_tests.app.page.cluster_list.locators import ClusterListLocators
 from tests.ui_tests.app.page.common.base_page import (
     BasePageObject,
     PageHeader,
     PageFooter,
 )
+from tests.ui_tests.app.page.common.table.page import CommonTableObj
 
 
 class ClusterListPage(BasePageObject):
@@ -23,3 +27,28 @@ class ClusterListPage(BasePageObject):
         super().__init__(driver, base_url, "/cluster")
         self.header = PageHeader(self.driver, self.base_url)
         self.footer = PageFooter(self.driver, self.base_url)
+        self.table = CommonTableObj(self.driver, self.base_url, ClusterListLocators.ClusterTable)
+
+    @allure.step("Create cluster from bundle")
+    def create_cluster(self, bundle: str, description: str = None, is_license: bool = False):
+        self.find_and_click(ClusterListLocators.Tooltip.cluster_add_btn)
+        popup = ClusterListLocators.CreateClusterPopup
+        self.wait_element_visible(popup.block)
+        self.find_element(popup.upload_bundle_btn).send_keys(bundle)
+        if description:
+            self.find_element(popup.description_input).send_keys(description)
+        self.find_and_click(popup.create_btn)
+        if is_license:
+            self.wait_element_visible(ClusterListLocators.LicensePopup.block)
+            self.find_and_click(ClusterListLocators.LicensePopup.agree_btn)
+
+    @allure.step("Get cluster info from row {row}")
+    def get_cluster_info_from_row(self, row: int) -> dict:
+        row_elements = ClusterListLocators.ClusterTable.ClusterRow
+        cluster_row = self.table.get_all_rows()[row]
+        return {
+            "name": self.find_child(cluster_row, row_elements.name).text,
+            "bundle": self.find_child(cluster_row, row_elements.bundle).text,
+            "description": self.find_child(cluster_row, row_elements.description).text,
+            "state": self.find_child(cluster_row, row_elements.state).text,
+        }
