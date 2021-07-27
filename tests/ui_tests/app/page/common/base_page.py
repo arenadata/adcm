984068d4262543af087308deb19d5e443acb3d89
Sealwing <egaraslanoff@yandex.ru>
2021-07-27 10:35:30 +0500

    liter fixes

--- tests/ui_tests/app/page/common/base_page.py
+++ tests/ui_tests/app/page/common/base_page.py
@@ -9,7 +9,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List
 
 import allure
 from adcm_pytest_plugin.utils import wait_until_step_succeeds
--- tests/ui_tests/test_host_page.py
+++ tests/ui_tests/test_host_page.py
@@ -50,7 +50,7 @@ def upload_and_create_provider(provider_bundle) -> Tuple[Bundle, Provider]:
 
 @pytest.fixture()
 @allure.title("Create host")
-def create_host(upload_and_create_provider: Tuple[Bundle, Provider]):
+def _create_host(upload_and_create_provider: Tuple[Bundle, Provider]):
     """Create default host using API"""
     provider = upload_and_create_provider[1]
     provider.host_create(HOST_FQDN)
@@ -58,7 +58,7 @@ def create_host(upload_and_create_provider: Tuple[Bundle, Provider]):
 
 @pytest.fixture()
 @allure.title("Create many hosts")
-def create_many_hosts(request, upload_and_create_provider):
+def _create_many_hosts(request, upload_and_create_provider):
     """Pass amount in param"""
     provider = upload_and_create_provider[1]
     for i in range(request.param):
@@ -167,11 +167,11 @@ def test_create_bonded_to_cluster_host(
     check_host_info(host_info, host_fqdn, PROVIDER_NAME, CLUSTER_NAME, 'created')
 
 
-@pytest.mark.full
+@pytest.mark.full()
 @pytest.mark.parametrize("create_many_hosts", [12], indirect=True)
 def test_host_list_pagination(
     page: HostListPage,
-    create_many_hosts,
+    _create_many_hosts,
 ):
     """Create more than 10 hosts and check pagination"""
     hosts_on_first_page, hosts_on_second_page = 10, 2
@@ -192,7 +192,7 @@ def test_host_list_pagination(
 
 def test_open_cluster_from_host_list(
     page: HostListPage,
-    create_host,
+    _create_host,
     upload_and_create_cluster: Tuple[Bundle, Provider],
 ):
     """Create host and go to cluster from host list"""
@@ -204,7 +204,7 @@ def test_open_cluster_from_host_list(
 
 
 @pytest.mark.parametrize(
-    'row_child_name, menu_item_name',
+    'row_child_name,menu_item_name',
     [
         pytest.param('fqdn', 'main', id='open_host_main'),
         pytest.param('status', 'status', id='open_status_menu', marks=pytest.mark.full),
@@ -215,7 +215,7 @@ def test_open_host_from_host_list(
     page: HostListPage,
     row_child_name: str,
     menu_item_name: str,
-    create_host,
+    _create_host,
 ):
     """Test open host page from host list"""
     row_child = getattr(HostListLocators.HostTable.HostRow, row_child_name)
@@ -244,12 +244,12 @@ def test_delete_host(
     page.wait_element_hide(HostListLocators.HostTable.row)
 
 
-@pytest.mark.full
+@pytest.mark.full()
 @pytest.mark.parametrize('menu', ['main', 'config', 'status', 'action'])
 def test_open_menu(
     upload_and_create_provider: Tuple[Bundle, Provider],
     upload_and_create_cluster,
-    create_host,
+    _create_host,
     page: HostListPage,
     menu: str,
 ):
@@ -258,7 +258,7 @@ def test_open_menu(
 
 
 def test_run_action_on_new_host(
-    create_host,
+    _create_host,
     page: HostListPage,
 ):
     """Create host and run action on it"""
@@ -268,7 +268,7 @@ def test_run_action_on_new_host(
 
 
 def test_run_action_from_menu(
-    create_host,
+    _create_host,
     page: HostListPage,
 ):
     """Run action from host actions menu"""
@@ -286,9 +286,9 @@ def test_run_action_from_menu(
         assert actions_before != actions_after, 'Action set did not change after "Init" action'
 
 
-@pytest.mark.full
+@pytest.mark.full()
 def test_filter_config(
-    create_host,
+    _create_host,
     page: HostListPage,
 ):
     """Use filters on host configuration page"""
@@ -324,7 +324,7 @@ def test_filter_config(
 
 
 def test_custom_name_config(
-    create_host,
+    _create_host,
     page: HostListPage,
 ):
     """Change configuration, save with custom name, compare changes"""
@@ -344,9 +344,9 @@ def test_custom_name_config(
         host_page.config.config_diff_is_presented('***', PASSWORD_FIELD_ADCM_TEST)
 
 
-@pytest.mark.full
+@pytest.mark.full()
 def test_reset_configuration(
-    create_host,
+    _create_host,
     page: HostListPage,
 ):
     """Change configuration, save, reset to defaults"""
@@ -363,9 +363,9 @@ def test_reset_configuration(
     assert password_value == '', 'Value in Password field should be empty after reset'
 
 
-@pytest.mark.full
+@pytest.mark.full()
 def test_field_validation(
-    create_host,
+    _create_host,
     page: HostListPage,
 ):
     """Inputs are validated correctly"""
@@ -377,8 +377,8 @@ def test_field_validation(
     host_page.config.check_field_is_invalid('Just item')
 
 
-@pytest.mark.full
-def test_open_adcm_main_menu(page, create_host):
+@pytest.mark.full()
+def test_open_adcm_main_menu(page, _create_host):
     """Open main menu by clicking on the menu icon in toolbar"""
     page.find_and_click(HostListLocators.Tooltip.apps_btn)
     AdminIntroPage(page.driver, page.base_url).wait_url_contains_path("/admin/intro")
