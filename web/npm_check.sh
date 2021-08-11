499598da210a2f345496353a44b2bc2839da3f06
nikolajgriskin <din.nick@bk.ru>
2021-08-11 14:20:00 +0500

    ADCM-1961 npm_check fixed

--- web/npm_check.sh
+++ web/npm_check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-yarn install npm-check
+yarn add npm-check
 export PATH=$PATH:./node_modules/.bin/
 yarn config set registry https://rt.adsw.io/artifactory/api/npm/arenadata-npm/
 yarn install --production
