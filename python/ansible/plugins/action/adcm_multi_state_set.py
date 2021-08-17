7c8f4c805fc78b565711c63d35305095678b567d
Talavrinova Sofya <tsd@arenadata.io>
2021-08-17 12:39:06 +0300

    ADCM-1965 fix ContextActionModule

--- python/ansible/plugins/action/adcm_multi_state_set.py
+++ python/ansible/plugins/action/adcm_multi_state_set.py
@@ -10,6 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=wrong-import-position, import-error
+
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
