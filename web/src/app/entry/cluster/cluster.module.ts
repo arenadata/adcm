794294aa083ecdc4481de81a65b85f04c845cc05
Aleksandr Lihih <sasha@awarenow.io>
2021-08-23 13:06:07 +0300

    [ADCM-1926] added group config for component

--- web/src/app/entry/cluster/cluster.module.ts
+++ web/src/app/entry/cluster/cluster.module.ts
@@ -112,8 +112,23 @@ const clusterRoutes: Routes = [
       { path: '', redirectTo: 'main', pathMatch: 'full' },
       { path: 'main', component: MainInfoComponent },
       { path: 'config', component: ConfigComponent },
+      { path: 'group_configs', component: ConfigGroupListComponent },
       { path: 'status', component: StatusComponent },
       { path: 'action', component: ActionCardComponent },
+    ],
+  },
+  {
+    path: ':cluster/service/:service/component/:servicecomponent/group_configs/:group_configs',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: DetailComponent,
+    data: {
+      entityService: CONFIG_GROUP_LIST_SERVICE
+    },
+    children: [
+      { path: '', redirectTo: 'host', pathMatch: 'full' },
+      { path: 'host', component: ConfigGroupHostListComponent },
+      { path: 'config', component: ConfigComponent, data: { isGroupConfig: true } },
     ],
   },
   {
