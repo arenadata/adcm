347916e4bac7426c43b453b0954d6c8ad63ddc55
Aleksandr Lihih <sasha@awarenow.io>
2021-09-01 11:17:18 +0300

    [ADCM-1926] added host_candidate for Group Config

--- web/src/app/config-groups/service/config-group-host-add.service.ts
+++ web/src/app/config-groups/service/config-group-host-add.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { FormModel, IAddService } from '../../shared/add-component/add-service-model';
+import { FormModel, IAddService } from '@app/shared/add-component/add-service-model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ClusterService } from '../../core/services/cluster.service';
+import { ClusterService } from '@app/core/services/cluster.service';
 import { Host, TypeName } from '../../core/types';
 import { convertToParamMap, Params } from '@angular/router';
 import { forkJoin, Observable } from 'rxjs';
-import { ApiService } from '../../core/api';
-import { environment } from '../../../environments/environment';
+import { ApiService } from '@app/core/api';
+import { environment } from '@env/environment';
 import { map } from 'rxjs/operators';
+import { ConfigGroup } from '@app/config-groups';
 
 const newConfigGroupHostForm = () =>
   new FormGroup({
@@ -44,8 +45,9 @@ export class ConfigGroupHostAddService implements IAddService {
 
   getList(type: TypeName, param: Params = {}): Observable<any[]> {
     const paramMap = convertToParamMap(param);
+    const current = this.Current as unknown as ConfigGroup;
 
-    return this.api.getList<Host>(`${environment.apiRoot}host/`, paramMap).pipe(
+    return this.api.getList<Host>(current.host_candidate, paramMap).pipe(
       map(({ results }) => results.map((host) => ({ ...host, name: host.fqdn })))
     );
   }
