fcb0a0ff4a2729a76769c350dc2f5a14e47b24b6
Anastasia <78135053+annademina@users.noreply.github.com>
2021-09-06 12:42:33 +0300

    Adcm-2014 change xpath to css in locators (#1050)
    * ADCM-2014 fix locators
    
    * ADCM-2014 fix send_text_to_element

--- tests/ui_tests/app/page/login/page.py
+++ tests/ui_tests/app/page/login/page.py
@@ -63,9 +63,9 @@ class LoginPage(BasePageObject):
     def fill_login_user_form(self, username, password):
         self.wait_element_visible(LoginPageLocators.login_form_block)
         self.clear_by_keys(LoginPageLocators.login_input)
-        self.set_locator_value(locator=LoginPageLocators.login_input, value=username)
+        self.send_text_to_element(locator=LoginPageLocators.login_input, text=username)
         self.clear_by_keys(LoginPageLocators.password_input)
-        self.set_locator_value(locator=LoginPageLocators.password_input, value=password)
+        self.send_text_to_element(locator=LoginPageLocators.password_input, text=password)
 
     @allure.step('Login with {username}: {password}')
     def login_user(self, username, password):
