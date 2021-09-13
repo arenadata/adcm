ccd05bb3ce3cdc563f7372f1549ed2af1e987f56
Sealwing <egaraslanoff@yandex.ru>
2021-09-13 16:11:00 +0500

    Tests on action config expanded to test cluster and provider related objects

--- tests/functional/test_action_config.py
+++ tests/functional/test_action_config.py
@@ -16,77 +16,115 @@ import allure
 
 from coreapi.exceptions import ErrorMessage
 
-from adcm_client.objects import ADCMClient, Cluster
+from adcm_client.objects import ADCMClient, Cluster, Service, Component, Provider, Host
 from adcm_pytest_plugin import utils as plugin_utils
-from adcm_pytest_plugin.steps.actions import run_cluster_action_and_assert_result
+from adcm_pytest_plugin.steps.actions import (
+    run_cluster_action_and_assert_result,
+    run_service_action_and_assert_result,
+    run_component_action_and_assert_result,
+    run_provider_action_and_assert_result,
+    run_host_action_and_assert_result,
+)
+
 
 # pylint: disable=redefined-outer-name
+
+ACTION_MAP = {
+    Cluster: run_cluster_action_and_assert_result,
+    Service: run_service_action_and_assert_result,
+    Component: run_component_action_and_assert_result,
+    Provider: run_provider_action_and_assert_result,
+    Host: run_host_action_and_assert_result,
+}
 
 
 @pytest.fixture()
 def cluster(sdk_client_fs: ADCMClient) -> Cluster:
     """Get cluster"""
     uploaded_bundle = sdk_client_fs.upload_from_fs(plugin_utils.get_data_dir(__file__, "cluster"))
-    return uploaded_bundle.cluster_create('test_cluster')
+    cluster = uploaded_bundle.cluster_create('test_cluster')
+    cluster.service_add(name='test_service')
+    return cluster
 
 
-def test_config_not_presented(cluster: Cluster):
+@pytest.fixture()
+def provider(sdk_client_fs: ADCMClient) -> Provider:
+    """Get cluster"""
+    uploaded_bundle = sdk_client_fs.upload_from_fs(plugin_utils.get_data_dir(__file__, "provider"))
+    provider = uploaded_bundle.provider_create('test_cluster')
+    provider.host_create(fqdn='test-host')
+    return provider
+
+
+def test_config_not_presented(cluster: Cluster, provider: Provider, sdk_client_fs: ADCMClient):
     """
     Action's configuration not presented
     Run action with config
     """
-    expected_message = (
-        "The task includes an option with an undefined variable. "
-        "The error was: 'dict object' has no attribute 'config'"
-    )
-    with allure.step('Run action without config and expect it to fail with ansible error'):
-        task = run_cluster_action_and_assert_result(cluster, 'no_config', 'failed', config={'some_param': 1})
-    with allure.step('Check that stdout log contains error message'):
-        assert expected_message in (
-            log := task.job().log(type='stdout').content
-        ), f'stdout expected to contain message: {expected_message}\nBut log was: {log}'
+    job_count = len(sdk_client_fs.job_list())
+    component = (service := cluster.service()).component()
+    host = provider.host()
+    for obj in (cluster, service, component, provider, host):
+        with allure.step(f'Check no config with action on {obj.__class__.__name__}'):
+            with allure.step('Run action without config and expect it to fail with ansible error'):
+                try:
+                    ACTION_MAP[obj.__class__](obj, 'no_config', 'failed', config={'some_param': "1"})
+                except ErrorMessage as e:
+                    _check_error_message(e, 400)
+                else:
+                    raise AssertionError("Action run request should've failed")
+            with allure.step("Check that job wasn't launched"):
+                _check_job_count_equal_to(job_count, sdk_client_fs)
 
 
-def test_incorrect_config(cluster: Cluster, sdk_client_fs: ADCMClient):
+def test_incorrect_config(cluster: Cluster, provider: Provider, sdk_client_fs: ADCMClient):
     """
     Action's configuration not presented
     Run action with "incorrect" config
     """
     job_count = len(sdk_client_fs.job_list())
-    with allure.step('Run action with "incorrect" config and expect ADCM response with non 500 status code'):
-        try:
-            run_cluster_action_and_assert_result(cluster, 'with_config', 'failed', config={'no_such_param': 1})
-        except ErrorMessage as e:
-            _check_error_message(e)
-        else:
-            raise AssertionError("Action should've failed")
-    with allure.step("Check that job wasn't launched"):
-        _check_job_count_equal_to(job_count, sdk_client_fs)
+    component = (service := cluster.service()).component()
+    host = provider.host()
+    for obj in (cluster, service, component, provider, host):
+        with allure.step(f'Check "incorrect" config with action on {obj.__class__.__name__}'):
+            with allure.step('Run action with "incorrect" config and expect ADCM response with non 500 status code'):
+                try:
+                    ACTION_MAP[obj.__class__](obj, 'with_config', 'failed', config={'no_such_param': "1"})
+                except ErrorMessage as e:
+                    _check_error_message(e, 400)
+                else:
+                    raise AssertionError("Action should've failed")
+            with allure.step("Check that job wasn't launched"):
+                _check_job_count_equal_to(job_count, sdk_client_fs)
 
 
-def test_pass_no_config(cluster: Cluster, sdk_client_fs: ADCMClient):
+def test_pass_no_config(cluster: Cluster, provider: Provider, sdk_client_fs: ADCMClient):
     """
     Action has config
     Run action without config
     """
     job_count = len(sdk_client_fs.job_list())
-    with allure.step(
-        'Run action that required config without config and expect ADCM to response with non 500 status code'
-    ):
-        try:
-            run_cluster_action_and_assert_result(cluster, 'with_config', 'failed')
-        except ErrorMessage as e:
-            _check_error_message(e)
-        else:
-            raise AssertionError("Action run should've failed")
-    with allure.step("Check that job wasn't launched"):
-        _check_job_count_equal_to(job_count, sdk_client_fs)
+    component = (service := cluster.service()).component()
+    host = provider.host()
+    for obj in (cluster, service, component, provider, host):
+        with allure.step(f'Check run action that require config with on {obj.__class__.__name__}'):
+            with allure.step(
+                'Run action that require config without config and expect ADCM to response with non 500 status code'
+            ):
+                try:
+                    ACTION_MAP[obj.__class__](obj, 'with_config', 'failed')
+                except ErrorMessage as e:
+                    _check_error_message(e, 400)
+                else:
+                    raise AssertionError("Action run should've failed")
+            with allure.step("Check that job wasn't launched"):
+                _check_job_count_equal_to(job_count, sdk_client_fs)
 
 
-def _check_error_message(error: ErrorMessage):
+def _check_error_message(error: ErrorMessage, expected_code: int):
     """Check status code in error message is less than 500"""
     status_code = _extract_status_code(error)
-    assert 400 <= status_code < 500, f"Response expected to have less than 500 status code, but {status_code} was found"
+    assert status_code == expected_code, f"Response expected to have {expected_code} status code, not {status_code}"
 
 
 def _check_job_count_equal_to(count: int, adcm_client: ADCMClient):
