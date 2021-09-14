a8dea84d81481ae90539fe69c16104502ab94c03
Ann <dan@arenadata.io>
2021-09-14 11:02:28 +0300

    ADCM-2043_1 fix pylint

--- tests/functional/test_groups.py
+++ tests/functional/test_groups.py
@@ -9,7 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name, unused-argument, duplicate-code
 
 import allure
 import pytest
