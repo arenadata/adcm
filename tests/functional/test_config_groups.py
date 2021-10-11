c963b07d4e9063fc340067f1aeac7497654e7688
Ann <dan@arenadata.io>
2021-10-11 09:49:23 +0300

    ADCM-2086 fix pylint

--- tests/functional/test_config_groups.py
+++ tests/functional/test_config_groups.py
@@ -9,7 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=redefined-outer-name, unused-argument, duplicate-code, no-self-use
+# pylint: disable=redefined-outer-name, unused-argument, duplicate-code, no-self-use, dangerous-default-value
 
 """Tests for config groups"""
 
