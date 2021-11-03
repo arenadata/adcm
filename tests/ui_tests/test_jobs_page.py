6176fa98d0977d30fd9ab3b6e6c6cb7d78a0f891
skhomuti <skhomuti@gmail.com>
2021-11-03 16:55:54 +0500

    ADCM-2244 pylint

--- tests/ui_tests/test_jobs_page.py
+++ tests/ui_tests/test_jobs_page.py
@@ -390,7 +390,7 @@ class TestTaskHeaderPopup:
         cluster_page.wait_config_loaded()
         # TODO remove sleep after fix ADCM-2279
         # the current sleep step is a temporary solution and need to fix flaky test evidence
-        import time
+        import time  # pylint: disable=import-outside-toplevel
         time.sleep(2)
         for action_name, expected_status in job_info['action_name'].items():
             if action_name == LONG_ACTION_DISPLAY_NAME:
