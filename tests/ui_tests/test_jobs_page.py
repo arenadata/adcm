6d0540c7e10f76bba70bf815731227f8179959b2
Sealwing <egaraslanoff@yandex.ru>
2021-11-08 12:39:10 +0500

    ADCM-2244 Fix linters

--- tests/ui_tests/test_jobs_page.py
+++ tests/ui_tests/test_jobs_page.py
@@ -391,6 +391,7 @@ class TestTaskHeaderPopup:
         # TODO remove sleep after fix ADCM-2279
         # the current sleep step is a temporary solution and need to fix flaky test evidence
         import time  # pylint: disable=import-outside-toplevel
+
         time.sleep(2)
         for action_name, expected_status in job_info['action_name'].items():
             if action_name == LONG_ACTION_DISPLAY_NAME:
