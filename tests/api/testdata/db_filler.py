76c8c1c8c635e3232934590f7c4ead2345206cec
Sergey Khomutinin <31664571+skhomuti@users.noreply.github.com>
2021-11-30 12:55:57 +0500

    ADCM-2371 enable /rbac/group endpoint api tests (#1308)
    * ADCM-2371 enable /rbac/group endpoint api tests
    Add new field type - password
    Fix body checks - add password support and disable fk checks
    Fix recursive test data generation
    
    * ADCM-2371 Fix error with endpoint changed value between test cases
    
    * ADCM-2371 extend clear context on data preparation for the list methods

--- tests/api/testdata/db_filler.py
+++ tests/api/testdata/db_filler.py
@@ -42,12 +42,13 @@ from tests.api.utils.api_objects import ADCMTestApiWrapper
 class DbFiller:
     """Utils to prepare data in DB before test"""
 
-    __slots__ = ("adcm", "_available_fkeys", "_used_fkeys")
+    __slots__ = ("adcm", "_available_fkeys", "_used_fkeys", "_endpoints_stack")
 
     def __init__(self, adcm: ADCMTestApiWrapper):
         self.adcm = adcm
         self._available_fkeys = defaultdict(set)
         self._used_fkeys = {}
+        self._endpoints_stack = []
 
     @allure.step("Generate valid request data")
     def generate_valid_request_data(self, endpoint: Endpoints, method: Methods) -> dict:
@@ -103,11 +104,21 @@ class DbFiller:
             return result
         raise ValueError(f"No such method {method}")
 
+    def _clear_context(self):
+        """
+        Clear the current db filler context
+        It should be called after an endpoint's data generation cycle
+        """
+        self._available_fkeys = defaultdict(set)
+        self._used_fkeys = {}
+        self._endpoints_stack = []
+        Endpoints.clear_endpoints_paths()
+
     def _get_or_create_data_for_endpoint(self, endpoint: Endpoints, force=False, prepare_data_only=False):
         """
         Get data for endpoint with data preparation
         """
-
+        self._endpoints_stack.append(endpoint)
         for data_class in endpoint.data_class.predefined_dependencies:
             # If this is 'object' endpoint (for example, group-config/{id}/host)
             # we don't force create object and try to get one (first) of current created
@@ -149,6 +160,8 @@ class DbFiller:
             if "{id}" not in fk_endpoint.path:
                 fk_data = get_endpoint_data(adcm=self.adcm, endpoint=fk_endpoint)
             if not fk_data or force:
+                if fk_endpoint in self._endpoints_stack:
+                    return []
                 fk_data = self._get_or_create_data_for_endpoint(endpoint=fk_endpoint, force=force)
             return self._choose_fk_field_value(field=field, fk_data=fk_data)
         return self._generate_field_value(field=field)
@@ -215,15 +228,14 @@ class DbFiller:
         current_ep_data = get_endpoint_data(adcm=self.adcm, endpoint=endpoint)
         if len(current_ep_data) < count:
             for _ in range(count - len(current_ep_data)):
-                # clean up context before generating new element
-                self._available_fkeys = defaultdict(set)
-                self._used_fkeys = {}
-                self._get_or_create_data_for_endpoint(
-                    endpoint=endpoint,
-                    force=True,
-                )
-                if len(get_endpoint_data(adcm=self.adcm, endpoint=endpoint)) > count:
-                    break
+                with allure.step(f"Create {endpoint.path} data"):
+                    self._clear_context()
+                    self._get_or_create_data_for_endpoint(
+                        endpoint=endpoint,
+                        force=True,
+                    )
+                    if len(get_endpoint_data(adcm=self.adcm, endpoint=endpoint)) > count:
+                        break
 
     @staticmethod
     def _generate_field_value(field: Field, old_value=None):
