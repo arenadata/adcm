1315fc548bcf080ec880b677b89fa0fcbb6c1f96
Ann <dan@arenadata.io>
2021-12-02 20:55:30 +0300

    ADCM-2374_1 fix remarks

--- tests/ui_tests/test_activatable_groups_ui_options.py
+++ tests/ui_tests/test_activatable_groups_ui_options.py
@@ -9,6 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# pylint: disable=redefined-outer-name,no-self-use,unused-argument,too-many-public-methods
 
 """Tests for activatable groups"""
 
