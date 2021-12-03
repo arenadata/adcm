40354500b1293622476e06f7e318e5501a4e5d00
Talavrinova Sofya <tsd@arenadata.io>
2021-12-03 13:20:02 +0300

    ADCM-2401 change enum realization

--- python/rbac/models.py
+++ python/rbac/models.py
@@ -67,6 +67,12 @@ class Group(AuthGroup):
     description = models.CharField(max_length=255, null=True)
 
 
+class RoleTypes(models.TextChoices):
+    business = 'business', 'business'
+    role = 'role', 'role'
+    hidden = 'hidden', 'hidden'
+
+
 class Role(models.Model):
     """
     Role is a list of Django permissions.
@@ -74,13 +80,7 @@ class Role(models.Model):
     Also Role can have children and so produce acyclic graph of linked roles
     """
 
-    ROLE_TYPES = [
-        ('business', 'business'),
-        ('role', 'role'),
-        ('hidden', 'hidden'),
-    ]
     name = models.CharField(max_length=160)
-#    display_name = models.CharField(max_length=160)
     description = models.TextField(blank=True)
     child = models.ManyToManyField("self", symmetrical=False, blank=True)
     permissions = models.ManyToManyField(Permission, blank=True)
@@ -89,7 +89,7 @@ class Role(models.Model):
     init_params = models.JSONField(default=dict)
     bundle = models.ForeignKey(Bundle, on_delete=models.CASCADE, null=True, default=None)
     built_in = models.BooleanField(default=True, null=False)
-    type = models.CharField(max_length=32, choices=ROLE_TYPES, null=False, default='business')
+    type = models.CharField(max_length=32, choices=RoleTypes.choices, null=False, default=RoleTypes.role)
     category = models.JSONField(default=list, null=False, validators=[validate_category])
     parametrized_by_type = models.JSONField(
         default=list, null=False, validators=[validate_object_type]
