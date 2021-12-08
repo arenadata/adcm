878bdce6d2cebb73b9560b66d9e95c466faac715
Talavrinova Sofya <tsd@arenadata.io>
2021-12-08 10:32:34 +0300

    ADCM-2392 fix inconsistency in role name, parametrized and built_in

--- python/rbac/endpoints/policy/views.py
+++ python/rbac/endpoints/policy/views.py
@@ -124,9 +124,11 @@ class PolicyViewSet(ModelPermViewSet):  # pylint: disable=too-many-ancestors
     ordering_fields = ('id', 'name', 'built_in', 'role')
 
     def create(self, request, *args, **kwargs):
+        built_in = request.data.get('built_in', False)
+        if built_in:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-
             policy = policy_create(**serializer.validated_data)
 
             return Response(data=self.get_serializer(policy).data, status=status.HTTP_201_CREATED)
