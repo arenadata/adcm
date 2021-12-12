2d32c01aa36c5deef39d49cc68657818fc22d804
Aleksandr Lihih <sasha@awarenow.io>
2021-12-12 17:05:39 +0300

    ADCM-2270 it is adds params(filters) for the rbac permissions requests

--- web/src/app/components/rbac/role-form/options/rbac-roles-as-options.directive.ts
+++ web/src/app/components/rbac/role-form/options/rbac-roles-as-options.directive.ts
@@ -1,15 +1,26 @@
-import { Directive, Inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Directive, Inject, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { AdwpStringHandler } from '@adwp-ui/widgets';
 import { RbacRoleModel } from '../../../../models/rbac/rbac-role.model';
 import { RbacRoleService } from '../../../../services/rbac-role.service';
 import { AdwpHandler } from '../../../../../../../../adwp_ui/projects/widgets/src/lib/cdk';
+import { Params } from '@angular/router';
+import { debounceTime, switchMap } from 'rxjs/operators';
+
+const RBAC_ROLES_INITIAL_PARAMS: Params = {
+  type: 'business'
+};
 
 @Directive({
-  selector: '[appRbacRolesAsOptions]',
+  selector: '[appRbacRolesAsOptions], [rbac-roles-as-options]',
   exportAs: 'rbacRoles'
 })
-export class RbacRolesAsOptionsDirective {
+export class RbacRolesAsOptionsDirective implements OnChanges {
+  @Input('rbac-roles-as-options')
+  params: Params;
+
+  private _params$: BehaviorSubject<Params>;
+
   options$: Observable<RbacRoleModel[]>;
 
   id: AdwpStringHandler<RbacRoleModel> = (item: RbacRoleModel) => String(item.id);
@@ -19,6 +30,25 @@ export class RbacRolesAsOptionsDirective {
   category: AdwpHandler<RbacRoleModel, string[]> = (item: RbacRoleModel) => item.category;
 
   constructor(@Inject(RbacRoleService) public service: RbacRoleService) {
-    this.options$ = service.getList({ type: 'business' });
+    this._params$ = new BehaviorSubject<Params>(RBAC_ROLES_INITIAL_PARAMS);
+
+    this.options$ = this._params$.pipe(
+      debounceTime(300),
+      switchMap((params) => service.getList(params))
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.handleParamsChanges(changes['params']);
+  }
+
+  private handleParamsChanges(params: SimpleChange): void {
+    if (params && params.currentValue) {
+      this._params$.next({
+        ...RBAC_ROLES_INITIAL_PARAMS,
+        ...this._params$.getValue(),
+        ...params.currentValue
+      });
+    }
   }
 }
