b40d3b91410153da30f7c2335e594c81ea522122
Nikolay Agarin <anv@arenadata.io>
2021-12-20 14:16:02 +0300

    ADCM-2429: add role filtering by category and category list action

--- python/rbac/endpoints/role/views.py
+++ python/rbac/endpoints/role/views.py
@@ -10,11 +10,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from django.db.models import Q
+from django_filters import rest_framework as filters
 from rest_flex_fields.serializers import FlexFieldsSerializerMixin
 from rest_framework import serializers
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
+from cm.models import ProductCategory
 from rbac.models import Role
 from rbac.services.role import role_create, role_update
 from rbac.utils import BaseRelatedSerializer
@@ -31,6 +35,7 @@ class RoleSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer):
     child = RoleChildSerializer(many=True, required=False)
     name = serializers.RegexField(r'^[^\n]*$', max_length=160, required=False)
     display_name = serializers.RegexField(r'^[^\n]*$', max_length=160, required=True)
+    category = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Role
@@ -45,27 +50,47 @@ class RoleSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer):
             'parametrized_by_type',
             'child',
             'url',
+            'any_category',
         )
         extra_kwargs = {
             'parametrized_by_type': {'required': True},
             'built_in': {'read_only': True},
             'type': {'read_only': True},
+            'any_category': {'read_only': True},
         }
         expandable_fields = {'child': ('rbac.endpoints.role.views.RoleSerializer', {'many': True})}
+
+    def get_category(self, obj):
+        return [c.value for c in obj.category.all()]
+
+
+class _CategoryFilter(filters.CharFilter):
+    def filter(self, qs, value):
+        if value:
+            qs = qs.filter(Q(category__value=value) | Q(any_category=True))
+        return qs
+
+
+class RoleFilter(filters.FilterSet):
+    category = _CategoryFilter()
+
+    class Meta:
+        model = Role
+        fields = (
+            'id',
+            'name',
+            'display_name',
+            'built_in',
+            'type',
+            'child',
+        )
 
 
 class RoleView(ModelPermViewSet):  # pylint: disable=too-many-ancestors
 
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
-    filterset_fields = (
-        'id',
-        'name',
-        'display_name',
-        'built_in',
-        'type',
-        'child',
-    )
+    filterset_class = RoleFilter
     ordering_fields = ('id', 'name', 'display_name', 'built_in', 'type')
     search_fields = ('name', 'display_name')
 
@@ -101,3 +126,7 @@ class RoleView(ModelPermViewSet):  # pylint: disable=too-many-ancestors
         if instance.built_in:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
+
+    @action(methods=['get'], detail=False)
+    def category(self, request):
+        return Response(sorted(b.value for b in ProductCategory.objects.all()))
