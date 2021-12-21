b3b2a397d1977a2b63e2c2ab6ee5746bca39e5ab
Ann <dan@arenadata.io>
2021-12-22 02:58:39 +0300

    ADCM-2455 fix remarks

--- tests/ui_tests/test_admin_page.py
+++ tests/ui_tests/test_admin_page.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name,no-self-use,too-few-public-methods
 
 """UI tests for /admin page"""
 
