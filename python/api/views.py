d463aebad8053cac71f3c9cb18c4f80fe498768e
Talavrinova Sofya <tsd@arenadata.io>
2021-12-22 18:04:17 +0300

    fix linters

--- python/api/views.py
+++ python/api/views.py
@@ -12,7 +12,6 @@
 
 import rest_framework
 from rest_framework import routers
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
