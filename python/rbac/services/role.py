af2e0d840dac7067d6f81066452f9e34e3e03b36
Talavrinova Sofya <tsd@arenadata.io>
2021-12-24 12:43:55 +0300

    ADCM-2449 add sum of parametrized_by param from child roles

--- python/rbac/services/role.py
+++ python/rbac/services/role.py
@@ -18,6 +18,28 @@ from rbac.models import Role, RoleTypes
 from rbac.utils import update_m2m_field
 
 
+def set_parametrized_from_child(role, children: List[Role]):
+    param_list = []
+
+    for child in children:
+        child_params = child.parametrized_by_type
+        for child_param in child_params:
+            if not check_child_parametrized(child_param, param_list):
+                param_list.append(child_param)
+    role.parametrized_by_type = param_list
+    role.save()
+
+
+def check_child_parametrized(child_param, param_list) -> bool:
+    cluster_hierarchy = {'cluster', 'service', 'component'}
+    if ((child_param == 'provider') and bool(cluster_hierarchy & set(param_list))) or (
+        (child_param in cluster_hierarchy) and ('provider' in param_list)
+    ):
+        errors = {'child': ['This children parametrized by types from different hierarchy']}
+        raise ValidationError(errors)
+    return child_param in param_list
+
+
 def check_role_child(child: List[Role]) -> None:
     for item in child:
         if not item.built_in:
@@ -44,6 +66,7 @@ def role_create(built_in=False, type_of_role=RoleTypes.role, **kwargs) -> Role:
         class_name='ParentRole',
         **kwargs,
     )
+    set_parametrized_from_child(role, child)
     role.child.add(*child)
     return role
 
@@ -58,6 +81,7 @@ def role_update(role: Role, **kwargs) -> Role:
     role.save()
 
     if child is not None:
+        set_parametrized_from_child(role, child)
         update_m2m_field(role.child, child)
 
     return role
