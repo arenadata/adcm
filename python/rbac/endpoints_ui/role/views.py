4047a4ee1ee003e42d69d2816a375061ff374dd7
Nikolay Agarin <anv@arenadata.io>
2021-12-24 10:21:49 +0300

    ADCM-2507: action for role object candidates

--- python/rbac/endpoints_ui/role/views.py
+++ python/rbac/endpoints_ui/role/views.py
@@ -12,10 +12,13 @@
 
 """Role object candidates view set"""
 
+from collections import defaultdict
+
 from rest_framework import serializers, mixins
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
+from cm import models as cm_models
 from rbac import models
 from rbac.viewsets import GenericPermViewSet
 
@@ -36,10 +39,67 @@ class RoleViewSet(mixins.ListModelMixin, GenericPermViewSet):
     @action(methods=['get'], detail=True)
     def object_candidate(self, request, **kwargs):
         role = self.get_object()
-        data = {
-            'cluster': [role.pk],
-            'service': [],
-            'provider': [],
-            'host': [],
-        }
-        return Response(data)
+        clusters = []
+        providers = []
+        services = []
+        hosts = []
+
+        if models.ObjectType.cluster.value in role.parametrized_by_type:
+            for cluster in cm_models.Cluster.objects.all():
+                clusters.append(
+                    {
+                        'name': cluster.display_name,
+                        'type': cm_models.PrototypeEnum.Cluster.value,
+                        'id': cluster.id,
+                    }
+                )
+
+        if models.ObjectType.provider.value in role.parametrized_by_type:
+            for provider in cm_models.HostProvider.objects.all():
+                providers.append(
+                    {
+                        'name': provider.display_name,
+                        'type': cm_models.PrototypeEnum.Provider.value,
+                        'id': provider.id,
+                    }
+                )
+
+        if models.ObjectType.host.value in role.parametrized_by_type:
+            for host in cm_models.Host.objects.all():
+                hosts.append(
+                    {
+                        'name': host.display_name,
+                        'type': cm_models.PrototypeEnum.Host.value,
+                        'id': host.id,
+                    }
+                )
+
+        if (
+            models.ObjectType.service.value in role.parametrized_by_type
+            or models.ObjectType.component.value in role.parametrized_by_type
+        ):
+            _services = defaultdict(list)
+            for service in cm_models.ClusterObject.objects.all():
+                _services[service.display_name].append(
+                    {
+                        'name': service.cluster.display_name,
+                        'type': 'service',
+                        'id': service.id,
+                    }
+                )
+            for service_name, clusters_info in _services.items():
+                services.append(
+                    {
+                        'name': service_name,
+                        'clusters': sorted(clusters_info, key=lambda x: x['name']),
+                    }
+                )
+
+        return Response(
+            {
+                'cluster': sorted(clusters, key=lambda x: x['name']),
+                'provider': sorted(providers, key=lambda x: x['name']),
+                'service': sorted(services, key=lambda x: x['name']),
+                'host': sorted(hosts, key=lambda x: x['name']),
+            }
+        )
