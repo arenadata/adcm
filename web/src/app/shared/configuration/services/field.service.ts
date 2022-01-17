4f4da8f1368f51619558ac1db1c135654aae4c8f
nikolajgriskin <din.nick@bk.ru>
2022-01-17 18:06:36 +0500

    ADCM-2417 fix form configuration

--- web/src/app/shared/configuration/services/field.service.ts
+++ web/src/app/shared/configuration/services/field.service.ts
@@ -317,9 +317,9 @@ export class FieldService {
   }
 
   checkValue(value: resultTypes, type: TNForm): resultTypes {
-    if ((value === '' || isEmptyObject(value)) && type === 'map') return {};
+    if ((value === '' || isEmptyObject(value) || value === null) && type === 'map') return {};
 
-    if ((value === '' || isEmptyObject(value)) && type === 'list') return [];
+    if ((value === '' || isEmptyObject(value) || value === null) && type === 'list') return [];
 
     if (value === '' || value === null || isEmptyObject(value)) return null;
 
