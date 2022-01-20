e3fb0ce8a0f4deba42135a1305c527f912dc0fd8
Talavrinova Sofya <tsd@arenadata.io>
2022-01-20 10:34:03 +0300

    ADCM-2557 review fix

--- python/api/action/views.py
+++ python/api/action/views.py
@@ -151,10 +151,7 @@ class RunTask(GenericAPIPermView):
         if action.host_action:
             obj = get_host_object(action, obj.cluster)
 
-        if not user.has_perm(f'cm.run_action_{action.name}', obj):
-            return False
-
-        return True
+        return user.has_perm(f'cm.run_action_{action.name}', obj)
 
     def check_action_perm(self, action, obj):
         if not self.has_action_perm(action, obj):
