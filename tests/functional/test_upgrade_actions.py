db1fce0d9c849cb1c0f430f83ec2353a754b56f4
Ann <dan@arenadata.io>
2022-04-01 11:12:16 +0300

    ADCM-2720 add tests for upgrade actions

--- tests/functional/test_upgrade_actions.py
+++ tests/functional/test_upgrade_actions.py
@@ -30,33 +30,35 @@ from tests.library.assertions import sets_are_equal
 from tests.library.errorcodes import INVALID_UPGRADE_DEFINITION, INVALID_OBJECT_DEFINITION
 
 
-@parametrize_by_data_subdirs(__file__, 'validation', 'valid')
-def test_validation_succeed_on_upload(sdk_client_fs, path):
-    """Test that valid bundles with upgrade actions succeed to upload"""
-    verbose_bundle_name = os.path.basename(path).replace('_', ' ').capitalize()
-    with allure.step(f'Upload bundle "{verbose_bundle_name}" and expect it to succeed'), catch_failed(
-        ErrorMessage, f'Bundle "{verbose_bundle_name}" should be uploaded successfully'
-    ):
-        bundle = sdk_client_fs.upload_from_fs(path)
-        bundle.delete()
+class TestUploadValidation:
+    """Test cases when upload bundles"""
 
+    @parametrize_by_data_subdirs(__file__, 'validation', 'valid')
+    def test_validation_succeed_on_upload(self, sdk_client_fs, path):
+        """Test that valid bundles with upgrade actions succeed to upload"""
+        verbose_bundle_name = os.path.basename(path).replace('_', ' ').capitalize()
+        with allure.step(f'Upload bundle "{verbose_bundle_name}" and expect it to succeed'), catch_failed(
+            ErrorMessage, f'Bundle "{verbose_bundle_name}" should be uploaded successfully'
+        ):
+            bundle = sdk_client_fs.upload_from_fs(path)
+            bundle.delete()
 
-@pytest.mark.parametrize(
-    ('bundle_dir_name', 'expected_error'),
-    [
-        ('bundle_switch_in_regular_actions', INVALID_OBJECT_DEFINITION),
-        ('incorrect_internal_action', INVALID_UPGRADE_DEFINITION),
-        ('no_bundle_switch', INVALID_UPGRADE_DEFINITION),
-    ],
-)
-def test_validation_failed_on_upload(bundle_dir_name, expected_error, sdk_client_fs):
-    """Test that invalid bundles with upgrade actions fails to upload"""
-    verbose_bundle_name = bundle_dir_name.replace('_', ' ').capitalize()
-    invalid_bundle_file = get_data_dir(__file__, 'validation', 'invalid', bundle_dir_name)
-    with allure.step(f'Upload bundle "{verbose_bundle_name}" and expect upload to fail'):
-        with pytest.raises(ErrorMessage) as e:
-            sdk_client_fs.upload_from_fs(invalid_bundle_file)
-        expected_error.equal(e)
+    @pytest.mark.parametrize(
+        ('bundle_dir_name', 'expected_error'),
+        [
+            ('bundle_switch_in_regular_actions', INVALID_OBJECT_DEFINITION),
+            ('incorrect_internal_action', INVALID_UPGRADE_DEFINITION),
+            ('no_bundle_switch', INVALID_UPGRADE_DEFINITION),
+        ],
+    )
+    def test_validation_failed_on_upload(self, bundle_dir_name, expected_error, sdk_client_fs):
+        """Test that invalid bundles with upgrade actions fails to upload"""
+        verbose_bundle_name = bundle_dir_name.replace('_', ' ').capitalize()
+        invalid_bundle_file = get_data_dir(__file__, 'validation', 'invalid', bundle_dir_name)
+        with allure.step(f'Upload bundle "{verbose_bundle_name}" and expect upload to fail'):
+            with pytest.raises(ErrorMessage) as e:
+                sdk_client_fs.upload_from_fs(invalid_bundle_file)
+            expected_error.equal(e)
 
 
 class TestFailedUpgradeAction:
@@ -104,7 +106,7 @@ class TestFailedUpgradeAction:
         run_cluster_action_and_assert_result(old_cluster, restore_action_name)
         self._check_state(old_cluster, expected_state_after_restore)
 
-    @pytest.xfail("Waiting for requirements improvement")
+    @pytest.mark.xfail(reason="Waiting for requirements improvement")
     def test_fail_after_switch_without_on_fail(self, sdk_client_fs, old_cluster):
         """
         Test bundle action fails after bundle_switch was performed.
@@ -215,3 +217,45 @@ def _extract_configs(cluster: Cluster):
             for service in cluster.service_list()
         },
     }
+
+
+class TestUpgradeActionRelations:
+    """Test cases when upgrade action"""
+
+    @pytest.mark.parametrize(
+        ("folder_dir", "file_dir"),
+        [
+            ('upgrade_failures', 'before_switch'),
+            ('upgrade_success', 'after_switch'),
+        ],
+    )
+    def test_check_upgrade_actions_relations(self, sdk_client_fs, folder_dir, file_dir):
+        """
+        Test bundle action fails before bundle_switch was performed
+        """
+        bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__, folder_dir, 'old'))
+        cluster = bundle.cluster_create('Test Cluster for Upgrade')
+        sdk_client_fs.upload_from_fs(get_data_dir(__file__, folder_dir, file_dir))
+        with allure.step("Check cluster actions list before update"):
+            actions_before = cluster.action_list().data
+            assert len(actions_before) == 1, "Should be 1 action"
+            assert actions_before[0].display_name == "dummy_action", "Should be action 'dummy_action'"
+        cluster.upgrade().do().wait()
+        cluster.reread()
+        with allure.step("Check jobs"):
+            jobs = sdk_client_fs.job_list()
+            assert len(jobs) >= 3, "There are should be 3 or more jobs"
+            assert len(
+                set(['first action after switch', 'switch action', 'first_action'])
+                & set([j.display_name for j in jobs.data])
+            ), "Jobs names differ"
+            assert (
+                len([log for data in jobs.data for log in data.log_files]) >= 6
+            ), "There are should be 6 or more log files"
+        with allure.step("Check cluster actions list after update"):
+            actions_after = cluster.action_list().data
+            assert len(actions_after) == 2, "Should be 2 actions"
+            assert [action.display_name for action in actions_after] == [
+                'dummy_action',
+                'restore',
+            ], "Should be actions 'dummy_action' and 'restore'"
