c45fd60415aefebce305df3669be35aa813a228d
Ann <dan@arenadata.io>
2022-04-08 11:00:53 +0300

    ADCM-2747 fix remarks

--- tests/ui_tests/app/page/common/group_config_list/page.py
+++ tests/ui_tests/app/page/common/group_config_list/page.py
@@ -25,7 +25,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.remote.webdriver import WebElement
 
 from tests.ui_tests.app.page.common.base_page import BasePageObject
-from tests.ui_tests.app.page.common.configuration.locators import CommonConfigMenu
 from tests.ui_tests.app.page.common.dialogs_locators import DeleteDialog
 from tests.ui_tests.app.page.common.group_config_list.locators import GroupConfigListLocators
 
