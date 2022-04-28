5fbe657411c3bad44c837e40e514522a147c2a69
Sealwing <egaraslanoff@yandex.ru>
2022-04-28 11:25:01 +0500

    ADCM-2792 Expand test on service delete to check concerns on cluster

--- tests/functional/test_delete_service.py
+++ tests/functional/test_delete_service.py
@@ -11,17 +11,22 @@
 # limitations under the License.
 
 """Tests for service delete method"""
-
+import allure
 from adcm_client.objects import ADCMClient
 from adcm_pytest_plugin.utils import get_data_dir
 
 
 def test_delete_service(sdk_client_fs: ADCMClient):
-    """If host has NO component, than we can simple remove it from cluster.
-
-    :return:
+    """
+    If host has NO component, then we can simply remove it from cluster.
     """
     bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__))
     cluster = bundle.cluster_create("test")
     service = cluster.service_add(name="zookeeper")
-    service.delete()
+    cluster.reread()
+    with allure.step("Ensure there's a concern on cluster from service's config"):
+        assert len(cluster.concerns()) > 0, 'There should be a concern on cluster from config of the service'
+    with allure.step('Delete service'):
+        service.delete()
+    with allure.step('Ensure that concern is gone from cluster after service removal'):
+        assert len(cluster.concerns()) == 0, 'Concern on cluster should be removed alongside with the service'
