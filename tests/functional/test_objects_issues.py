cc3461fa1eb92199bb68b01af9053df03d9148ae
Sealwing <egaraslanoff@yandex.ru>
2022-05-05 16:56:17 +0500

    ADCM-2811 Test on concerns cleaning after the cluster is deleted

--- tests/functional/test_objects_issues.py
+++ tests/functional/test_objects_issues.py
@@ -17,9 +17,27 @@ import coreapi
 import pytest
 
 from adcm_client.base import ActionHasIssues
-from adcm_client.objects import ADCMClient
+from adcm_client.objects import ADCMClient, Provider
 from adcm_pytest_plugin import utils
+from adcm_pytest_plugin.utils import catch_failed
+from coreapi.exceptions import ErrorMessage
+
 from tests.library.errorcodes import UPGRADE_ERROR
+
+
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture()
+def provider_bundle(sdk_client_fs):
+    """Get provider without concerns bundle"""
+    return sdk_client_fs.upload_from_fs(utils.get_data_dir(__file__, 'provider_wo_concern'))
+
+
+@pytest.fixture()
+def provider(provider_bundle):
+    """Get provider without concerns"""
+    return provider_bundle.provider_create(name='Test Provider')
 
 
 def test_action_should_not_be_run_while_cluster_has_an_issue(sdk_client_fs: ADCMClient):
@@ -86,9 +104,7 @@ def test_when_hostprovider_has_issue_than_upgrade_locked(sdk_client_fs: ADCMClie
 
 
 @allure.link("https://jira.arenadata.io/browse/ADCM-487")
-def test_when_component_has_no_constraint_then_cluster_doesnt_have_issues(
-    sdk_client_fs: ADCMClient,
-):
+def test_when_component_has_no_constraint_then_cluster_doesnt_have_issues(sdk_client_fs: ADCMClient):
     """Test no cluster issues if no constraints on components"""
     with allure.step("Create cluster (component has no constraint)"):
         bundle_path = utils.get_data_dir(__file__, "cluster_component_hasnt_constraint")
@@ -100,3 +116,36 @@ def test_when_component_has_no_constraint_then_cluster_doesnt_have_issues(
     with allure.step("Check if state is always-locked"):
         cluster.reread()
         assert cluster.state == "always-locked"
+
+
+@allure.link('https://arenadata.atlassian.net/browse/ADCM-2810')
+def test_concerns_are_deleted_with_cluster_deletion(sdk_client_fs: ADCMClient, provider: Provider):
+    """Tests concerns are deleted from all related objects when the cluster is deleted"""
+    with allure.step('Upload bundles and create cluster'):
+        cluster_bundle = sdk_client_fs.upload_from_fs(utils.get_data_dir(__file__, 'cluster'))
+        cluster = cluster_bundle.cluster_create(name='Test Cluster')
+    with allure.step('Add service, map host to it and add another service'):
+        service_1 = cluster.service_add(name='service_1')
+        host_1, host_2 = [cluster.host_add(provider.host_create(f'test-host-{i}')) for i in range(2)]
+        cluster.hostcomponent_set((host_1, service_1.component()))
+        cluster.service_add(name='service_2')
+    with allure.step('Check there is a concern on one of the hosts'):
+        with catch_failed(ErrorMessage, 'No errors should be raised on concerns check'):
+            _check_object_has_concerns(host_1)
+            _check_object_has_no_concerns(host_2)
+    with allure.step('Delete cluster and expect issues to go away'):
+        cluster.delete()
+    with allure.step('Check concern is gone'):
+        with catch_failed(ErrorMessage, 'No errors should be raised on concerns check'):
+            _check_object_has_no_concerns(host_1)
+            _check_object_has_no_concerns(host_2)
+
+
+def _check_object_has_concerns(adcm_object):
+    adcm_object.reread()
+    assert len(adcm_object.concerns()) > 0, f'Object {adcm_object} should has at least one concern'
+
+
+def _check_object_has_no_concerns(adcm_object):
+    adcm_object.reread()
+    assert len(adcm_object.concerns()) == 0, f'Object {adcm_object} should not has any concerns'
