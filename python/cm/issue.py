4310de7cb783de52b55a9002fa7d128f64694500
dskrynnik <d.skrynnik@arenadata.io>
2022-05-09 15:02:17 +0300

    ADCM-2810 fix linters

--- python/cm/issue.py
+++ python/cm/issue.py
@@ -316,7 +316,7 @@ def update_hierarchy_issues(obj: ADCMEntity, remove_obj=False):
     if remove_obj:
         for concern in obj.concerns.all():
             if concern.owner == obj:
-                log.debug(f'deleting concern {concern}')
+                log.debug('deleting concern %s', concern)
                 concern.delete()
     else:
         for node in affected_nodes:
