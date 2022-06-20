29de720f2243791bc123d7b3ae2afd7adfa42c89
Sealwing <egaraslanoff@yandex.ru>
2022-06-20 15:43:03 +0500

    ADCM-2894 LDAP + RBAC basic tests

--- tests/library/ldap_interactions.py
+++ tests/library/ldap_interactions.py
@@ -16,6 +16,7 @@ import uuid
 from typing import NamedTuple, List
 from zlib import crc32
 
+import allure
 import ldap
 from adcm_pytest_plugin.custom_types import SecureString
 from ldap.ldapobject import SimpleLDAPObject
@@ -46,6 +47,10 @@ class LDAPEntityManager:
     _BASE_GROUP_MODLIST = [('objectClass', [b'top', b'group'])]
     _BASE_USER_MODLIST = [('objectClass', [b'top', b'person', b'organizationalPerson', b'user'])]
 
+    _ACTIVE_USER_UAC = b'512'  # regular user
+    _INACTIVE_USER_UAC = b'514'  # user, inactive
+    _DEFAULT_USER_UAC = b'546'  # user, inactive, password not required
+
     def __init__(self, config: LDAPTestConfig, test_name: str):
         ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)  # pylint: disable=no-member
         self.conn = ldap.initialize(config.uri)  # pylint: disable=no-member
@@ -70,6 +75,7 @@ class LDAPEntityManager:
         self.clean_test_ou()
         self.conn.unbind()
 
+    @allure.step('Create OU {name}')
     def create_ou(self, name: str, custom_base_dn: str = None) -> str:
         """Create OU (use for 'isolation' in tests)"""
         base_dn = custom_base_dn or self.test_dn
@@ -78,6 +84,7 @@ class LDAPEntityManager:
         self._created_records.append(new_dn)
         return new_dn
 
+    @allure.step('Create group {name}')
     def create_group(self, name: str, custom_base_dn: str = None) -> str:
         """Create group (CN) that can include users"""
         base_dn = custom_base_dn or self.test_dn
@@ -86,23 +93,54 @@ class LDAPEntityManager:
         self._created_records.append(new_dn)
         return new_dn
 
+    @allure.step('Create user {name} with password {password}')
     def create_user(self, name: str, password: str, custom_base_dn: str = None) -> str:
-        """Create user (CN)"""
+        """
+        Create user (CN) and activate it.
+        `name` is used both in CN and as sAMAccountName,
+        so it should be unique not only in parent OU.
+        """
         base_dn = custom_base_dn or self.test_dn
         new_dn = f'CN={name},{base_dn}'
         self.conn.add_s(new_dn, self._BASE_USER_MODLIST + [('sAMAccountName', name.encode('utf-8'))])
-        pass_utf16 = f'"{password}"'.encode('utf-16-le')
-        self.conn.modify_s(new_dn, [(ldap.MOD_REPLACE, 'unicodePwd', [pass_utf16])])  # pylint: disable=no-member
-        # activate user
-        self.conn.modify_s(new_dn, [(ldap.MOD_REPLACE, 'userAccountControl', b'512')])  # pylint: disable=no-member
         self._created_records.append(new_dn)
+        self.set_user_password(new_dn, password)
+        self.activate_user(new_dn)
         return new_dn
 
+    @allure.step('Activate user {user_dn}')
+    def activate_user(self, user_dn: str, uac: bytes = _ACTIVE_USER_UAC) -> None:
+        """Activate user"""
+        self.conn.modify_s(
+            user_dn, [(ldap.MOD_REPLACE, 'userAccountControl', uac)]  # pylint: disable=no-member
+        )
+
+    @allure.step('Deactivate user {user_dn}')
+    def deactivate_user(self, user_dn: str, uac: bytes = _INACTIVE_USER_UAC) -> None:
+        """Deactivate user"""
+        self.conn.modify_s(
+            user_dn, [(ldap.MOD_REPLACE, 'userAccountControl', uac)]  # pylint: disable=no-member
+        )
+
+    @allure.step('Set password "{password}" for user {user_dn}')
+    def set_user_password(self, user_dn: str, password: str) -> None:
+        """Set password for an existing user"""
+        pass_utf16 = f'"{password}"'.encode('utf-16-le')
+        self.conn.modify_s(user_dn, [(ldap.MOD_REPLACE, 'unicodePwd', [pass_utf16])])  # pylint: disable=no-member
+
+    @allure.step('Add user {user_dn} to {group_dn}')
     def add_user_to_group(self, user_dn: str, group_dn: str) -> None:
         """Add user to a group"""
         mod_group = [(ldap.MOD_ADD, 'member', [user_dn.encode('utf-8')])]  # pylint: disable=no-member
         self.conn.modify_s(group_dn, mod_group)
 
+    @allure.step('Remove user {user_dn} from {group_dn}')
+    def remove_user_from_group(self, user_dn: str, group_dn: str) -> None:
+        """Remove user from group"""
+        mod_group = [(ldap.MOD_DELETE, 'member', [user_dn.encode('utf-8')])]  # pylint: disable=no-member
+        self.conn.modify_s(group_dn, mod_group)
+
+    @allure.step('Cleat test OU')
     def clean_test_ou(self):
         """Remove every object in test OU"""
         if self.test_dn is None:
