13d4d96cf62925a06bc16eb91ddc51b58d1327e8
dbardin90 <d.bardin@arenadata.io>
2022-06-20 11:27:39 +0300

    ADCM-2836 - MR fixes

--- web/src/app/shared/components/upgrades/master/master.component.ts
+++ web/src/app/shared/components/upgrades/master/master.component.ts
@@ -63,7 +63,7 @@ export class UpgradeMasterComponent extends BaseDirective implements DynamicComp
   }
 
   isDisabled(value: IValue) {
-    return value && ((value.hostmap && value.hostmap.noValid) || (value.config && !value.config.form?.valid));
+    return value?.hostmap?.noValid || !value?.config?.form?.valid;
   }
 
   run(value: IValue = {}) {
