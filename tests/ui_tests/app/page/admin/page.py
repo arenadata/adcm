cdf53374d8039e4a26a639bab5872f6e66dc0e0c
Ann <dan@arenadata.io>
2022-06-22 12:59:21 +0300

    ADCM-2790 fix tests

--- tests/ui_tests/app/page/admin/page.py
+++ tests/ui_tests/app/page/admin/page.py
@@ -566,7 +566,6 @@ class AdminPoliciesPage(GeneralAdminPage):
     def fill_select_in_policy_popup(self, items, available_items_locator):
         for item in items.split(", "):
             self.wait_element_visible(available_items_locator)
-            self.wait_element_clickable(available_items_locator)
             for count, available_item in enumerate(self.find_elements(available_items_locator)):
                 if available_item.text == item:
                     available_item.click()
