2b8c5c12e1b725ce8a88b9e88ef8b3ca8eaedcc9
Sealwing <egaraslanoff@yandex.ru>
2022-07-07 18:15:02 +0500

    ADCM-2896 LDAP user deactivation/deletion tests

--- tests/functional/ldap_auth/test_sync.py
+++ tests/functional/ldap_auth/test_sync.py
@@ -12,15 +12,18 @@
 
 """Test synchronization and test connection with LDAP"""
 
+from contextlib import contextmanager
 from typing import Collection
 
 import allure
 import pytest
+import requests
 from adcm_client.objects import ADCMClient, Group
 from adcm_pytest_plugin.steps.actions import wait_for_task_and_assert_result
 
 from tests.functional.ldap_auth.utils import get_ldap_group_from_adcm, get_ldap_user_from_adcm
-from tests.library.assertions import sets_are_equal
+from tests.library.assertions import sets_are_equal, expect_no_api_error, expect_api_error
+from tests.library.errorcodes import UNAUTHORIZED
 
 pytestmark = [pytest.mark.usefixtures('configure_adcm_ldap_ad')]
 
@@ -55,6 +58,7 @@ def test_sync_with_already_existing_group(sdk_client_fs, ldap_user_in_group, lda
 
 
 def test_ldap_group_removed(sdk_client_fs, ldap_ad, ldap_group, ldap_user, ldap_user_in_group):
+    """Test LDAP group removed from ADCM after it's removed from LDAP"""
     _run_sync(sdk_client_fs)
     check_existing_users(sdk_client_fs, {ldap_user_in_group['name'], ldap_user['name']})
     check_existing_groups(sdk_client_fs, {ldap_group['name']})
@@ -65,6 +69,7 @@ def test_ldap_group_removed(sdk_client_fs, ldap_ad, ldap_group, ldap_user, ldap_
 
 
 def test_user_removed_from_group(sdk_client_fs, ldap_ad, ldap_group, ldap_user_in_group, ldap_user):
+    """Test that when user is removed from group in AD, it is also removed in ADCM's LDAP group"""
     another_group: Group = sdk_client_fs.group_create('Another group')
     _run_sync(sdk_client_fs)
     with allure.step('Add LDAP users to the local group'):
@@ -83,6 +88,39 @@ def test_user_removed_from_group(sdk_client_fs, ldap_ad, ldap_group, ldap_user_i
         assert len(group.user_list()) == 0, 'Group from LDAP should be empty'
         another_group.reread()
         assert len(another_group.user_list()) == 2, 'Local group should still have both users in it'
+
+
+def test_user_deactivated(sdk_client_fs, ldap_ad, ldap_user):
+    """Test that user is deactivated in ADCM after it's deactivated in AD"""
+    credentials = {'user': ldap_user['name'], 'password': ldap_user['password'], 'url': sdk_client_fs.url}
+    with allure.step('Run sync and check that user is active and can log in'):
+        _run_sync(sdk_client_fs)
+        user = get_ldap_user_from_adcm(sdk_client_fs, ldap_user['name'])
+        assert user.is_active, 'User should be active'
+        expect_no_api_error('login as LDAP active user', ADCMClient, **credentials)
+    with allure.step('Deactivate user in LDAP and check it is deactivated after sync'):
+        with session_should_expire(**credentials):
+            ldap_ad.deactivate_user(ldap_user['dn'])
+            _run_sync(sdk_client_fs)
+            user.reread()
+            assert not user.is_active, 'User should be deactivated'
+            expect_api_error('login as deactivated user', ADCMClient, **credentials)
+
+
+def test_user_deleted(sdk_client_fs, ldap_ad, ldap_user):
+    """Test that user is deleted in ADCM after it's deleted in AD"""
+    credentials = {'user': ldap_user['name'], 'password': ldap_user['password'], 'url': sdk_client_fs.url}
+    with allure.step('Run sync and check that user is active and can log in'):
+        _run_sync(sdk_client_fs)
+
+        check_existing_users(sdk_client_fs, {ldap_user['name']})
+        expect_no_api_error('login as LDAP user', ADCMClient, **credentials)
+    with allure.step('Delete user in LDAP and check access denied'):
+        with session_should_expire(**credentials):
+            ldap_ad.delete(ldap_user['dn'])
+            _run_sync(sdk_client_fs)
+            check_existing_users(sdk_client_fs)
+            expect_api_error('login as deleted user', ADCMClient, **credentials)
 
 
 @allure.step('Check users existing in ADCM')
@@ -113,6 +151,23 @@ def check_existing_groups(
         sets_are_equal(expected_ldap, existing_ldap, message='Not all LDAP groups are presented in ADCM')
     with allure.step('Check local groups'):
         sets_are_equal(existing_local, expected_local, message='Not all local groups are presented in ADCM')
+
+
+@contextmanager
+def session_should_expire(user: str, password: str, url: str):
+    """Check that session expires"""
+    with allure.step('Login via API'):
+        client = ADCMClient(url=url, user=user, password=password)
+    with requests.Session() as session:
+        with allure.step('Get session cookies'):
+            response = session.post(f'{url}/api/v1/rbac/token/', json={'username': user, 'password': password})
+            assert response.status_code == 200, 'Clusters page should be available'
+        yield
+        with allure.step('Check session is "over"'):
+            response = session.get(f'{url}/api/v1/cluster')
+            assert response.status_code == 401, 'Request to ADCM should fail with 401 status'
+    with allure.step('Check call via client is considered unauthorized'):
+        expect_api_error('get cluster list', client.cluster_list, err_=UNAUTHORIZED)
 
 
 @allure.step('Run LDAP sync action')
