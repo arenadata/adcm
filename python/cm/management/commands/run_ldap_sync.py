295d371ae52267ed413b249e9205e3d99f21c470
Talavrinova Sofya <tsd@arenadata.io>
2022-07-12 16:18:45 +0300

    ADCM-2961 add periodic run of sync action

---
+++ python/cm/management/commands/run_ldap_sync.py
@@ -0,0 +1,54 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import timedelta, datetime
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from cm.config import Job
+from cm.job import start_task
+from cm.models import ADCM, Action, ConfigLog, TaskLog
+from cm.logger import log_cron_task as log
+
+
+def get_settings(adcm_object):
+    current_configlog = ConfigLog.objects.get(
+        obj_ref=adcm_object.config, id=adcm_object.config.current
+    )
+    if current_configlog.attr['ldap_integration']['active']:
+        ldap_config = current_configlog.config['ldap_integration']
+        return ldap_config['sync_interval']
+    return 0
+
+
+class Command(BaseCommand):
+    help = 'Run synchronization with ldap if sync_interval is specified in ADCM settings'
+
+    def handle(self, *args, **options):
+        adcm_object = ADCM.objects.get(id=1)
+        period = get_settings(adcm_object)
+        if period <= 0:
+            return
+        if TaskLog.objects.filter(action__name='run_ldap_sync', status=Job.RUNNING).exists():
+            log.debug("Sync has already launched, we need to wait for the task end")
+            return
+        last_sync = TaskLog.objects.filter(
+            action__name='run_ldap_sync', status__in=[Job.SUCCESS, Job.FAILED]
+        ).last()
+        new_rotate_time = (
+            last_sync.finish_date - timedelta(minutes=1) if last_sync else datetime.min
+        ) + timedelta(minutes=period)
+        if new_rotate_time <= timezone.now():
+            log.debug("Ldap sync launched in %s", timezone.now())
+            action = Action.objects.get(name='run_ldap_sync', prototype=adcm_object.prototype)
+            start_task(action, adcm_object, {}, {}, [], [], False)
