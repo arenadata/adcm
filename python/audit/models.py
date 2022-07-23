a4f47d29406fae4852cd69425c7ee57bb416f78f
Konstantin Shestakov <winmasta@yandex.ru>
2022-07-23 14:35:16 +0700

    ADCM-2972 add group create audit

--- python/audit/models.py
+++ python/audit/models.py
@@ -142,4 +142,12 @@ AUDIT_OPERATION_MAP = {
             object_type=AuditObjectType.Role.label,
         ),
     },
+    "GroupViewSet": {
+        "POST": AuditOperation(
+            name=f"{AuditObjectType.Group.label.capitalize()} "
+            f"{AuditLogOperationType.Create.label}d",
+            operation_type=AuditLogOperationType.Create.label,
+            object_type=AuditObjectType.Group.label,
+        ),
+    },
 }
