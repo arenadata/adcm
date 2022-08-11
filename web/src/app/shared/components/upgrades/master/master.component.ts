3eddfec5861483f4299315a0322c95a6482ab842
dbardin90 <d.bardin@arenadata.io>
2022-08-11 16:34:40 +0300

    ADCM-2921 - test fix

--- web/src/app/shared/components/upgrades/master/master.component.ts
+++ web/src/app/shared/components/upgrades/master/master.component.ts
@@ -9,13 +9,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewChecked,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { DynamicComponent, DynamicEvent } from '@app/shared/directives/dynamic/dynamic.directive';
 import { BaseDirective } from '@app/shared/directives';
 import { IMasterData, IValue, MasterService, whatShow } from './master.service';
 import { IUpgrade } from "@app/shared/components";
 import { UpgradeParameters } from "@app/shared/components/upgrades/upgrade.directive";
 import { MatStepper } from "@angular/material/stepper";
+import {ChangeDetection} from "@angular/cli/lib/config/schema";
 
 @Component({
   selector: 'app-master',
@@ -24,7 +34,7 @@ import { MatStepper } from "@angular/material/stepper";
   providers: [MasterService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UpgradeMasterComponent extends BaseDirective implements DynamicComponent, OnInit {
+export class UpgradeMasterComponent extends BaseDirective implements DynamicComponent, OnInit, AfterViewChecked {
   event: EventEmitter<DynamicEvent> = new EventEmitter();
   model: UpgradeParameters;
   upgrade: IUpgrade;
@@ -34,12 +44,16 @@ export class UpgradeMasterComponent extends BaseDirective implements DynamicComp
 
   @ViewChild('runBtn', { read: ElementRef }) runBtn: ElementRef;
 
-  constructor(private service: MasterService) {
+  constructor(private service: MasterService, public cd: ChangeDetectorRef) {
     super();
   }
 
   ngOnInit(): void {
     if (this.model.upgrades?.length === 1) this.choose(this.model.upgrades[0]);
+  }
+
+  ngAfterViewChecked() {
+    this.cd.detectChanges();
   }
 
   choose(upgrade: IUpgrade) {
