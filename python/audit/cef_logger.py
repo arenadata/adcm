09d146afb2a3f8a606b159b781e10cabdd8dbb40
dskrynnik <danbalalan@gmail.com>
2022-08-12 17:03:24 +0300

    ADCM-3040 double quotes

--- python/audit/cef_logger.py
+++ python/audit/cef_logger.py
@@ -25,26 +25,26 @@ from audit.models import AuditLog, AuditLogOperationResult, AuditSession
 audit_log = logging.getLogger(AuditConfig.name)
 
 TEMPLATE = (
-    '{syslog_header}'
-    '{cef_version}|{device_vendor}|{device_product}|'
-    '{adcm_version}|{signature_id}|{name}|{severity}|{extension}'
+    "{syslog_header}"
+    "{cef_version}|{device_vendor}|{device_product}|"
+    "{adcm_version}|{signature_id}|{name}|{severity}|{extension}"
 )
 
 
 def get_adcm_version():
-    with open('/adcm/config.json', encoding='utf-8') as f:
-        return json.loads(f.read())['version']
+    with open("/adcm/config.json", encoding="utf-8") as f:
+        return json.loads(f.read())["version"]
 
 
 class CEFLogConstants:
-    syslog_header: str = ''
-    cef_version: str = 'CEF: 0'
-    device_vendor: str = 'Arenadata Software'
-    device_product: str = 'Arenadata Cluster Manager'
+    syslog_header: str = ""
+    cef_version: str = "CEF: 0"
+    device_vendor: str = "Arenadata Software"
+    device_product: str = "Arenadata Cluster Manager"
     adcm_version: str = get_adcm_version()
     severity: int = 1
-    operation_name_session: str = 'User logged'
-    extension_keys: Tuple[str] = ('actor', 'act', 'operation', 'resource', 'result', 'timestamp')
+    operation_name_session: str = "User logged"
+    extension_keys: Tuple[str] = ("actor", "act", "operation", "resource", "result", "timestamp")
 
 
 def cef_log(
@@ -54,25 +54,25 @@ def cef_log(
     empty_resource: bool = False,
 ) -> None:
     extension = OrderedDict.fromkeys(CEFLogConstants.extension_keys, None)
-    extension['timestamp'] = str(tz.now())
+    extension["timestamp"] = str(tz.now())
 
     if isinstance(audit_instance, AuditSession):
         operation_name = CEFLogConstants.operation_name_session
         if audit_instance.user:
-            extension['actor'] = audit_instance.user.username
+            extension["actor"] = audit_instance.user.username
         else:
-            extension['actor'] = audit_instance.login_details.get('username', '<undefined>')
-        extension['operation'] = operation_name
-        extension['result'] = audit_instance.login_result
+            extension["actor"] = audit_instance.login_details.get("username", "<undefined>")
+        extension["operation"] = operation_name
+        extension["result"] = audit_instance.login_result
 
     elif isinstance(audit_instance, AuditLog):
         operation_name = audit_instance.operation_name
-        extension['actor'] = audit_instance.user.username
-        extension['act'] = audit_instance.operation_type
-        extension['operation'] = operation_name
+        extension["actor"] = audit_instance.user.username
+        extension["act"] = audit_instance.operation_type
+        extension["operation"] = operation_name
         if not empty_resource and audit_instance.audit_object:
-            extension['resource'] = audit_instance.audit_object.object_name
-        extension['result'] = audit_instance.operation_result
+            extension["resource"] = audit_instance.audit_object.object_name
+        extension["result"] = audit_instance.operation_result
         if audit_instance.operation_result == AuditLogOperationResult.Denied:
             severity = 3
 
@@ -88,6 +88,6 @@ def cef_log(
         signature_id=signature_id,
         name=operation_name,
         severity=severity if severity is not None else CEFLogConstants.severity,
-        extension=' '.join([f'{k}={v}' for k, v in extension.items() if v is not None]),
+        extension=" ".join([f"{k}={v}" for k, v in extension.items() if v is not None]),
     )
     audit_log.info(msg)
