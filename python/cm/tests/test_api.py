179acfd8ce0f9718532c747d733d32443bfed737
dskrynnik <danbalalan@gmail.com>
2022-08-17 14:31:53 +0300

    ADCM-3074 fix test_cluster_host

--- python/cm/tests/test_api.py
+++ python/cm/tests/test_api.py
@@ -218,13 +218,15 @@ class TestAPI(TestBase):  # pylint: disable=too-many-public-methods
         name = name or uuid4().hex
         ssh_bundle_id, host_proto = self.get_host_proto_id()
         _, provider_proto = self.get_host_provider_proto_id()
-        response = self.api_post('/provider/', {'name': name, 'prototype_id': provider_proto})
-        self.assertEqual(response.status_code, 201, msg=response.text)
-        provider_id = response.json()['id']
-        response = self.api_post(
-            '/host/', {'fqdn': fqdn, 'prototype_id': host_proto, 'provider_id': provider_id}
+        response = self.client.post(
+            reverse('provider'), {'name': name, 'prototype_id': provider_proto}
         )
-        self.assertEqual(response.status_code, 201, msg=response.text)
+        self.assertEqual(response.status_code, 201, msg=response.content)
+        provider_id = response.json()['id']
+        response = self.client.post(
+            reverse('host'), {'fqdn': fqdn, 'prototype_id': host_proto, 'provider_id': provider_id}
+        )
+        self.assertEqual(response.status_code, 201, msg=response.content)
         host_id = response.json()['id']
         return (ssh_bundle_id, provider_id, host_id)
 
@@ -512,54 +514,66 @@ class TestAPI(TestBase):  # pylint: disable=too-many-public-methods
         self.assertEqual(response.status_code, 204, msg=response.content)
 
     def test_cluster_host(self):
-        response = self.api_post('/stack/load/', {'bundle_file': self.adh_bundle})
-        self.assertEqual(response.status_code, 200, msg=response.text)
-        response = self.api_post('/stack/load/', {'bundle_file': self.ssh_bundle})
-        self.assertEqual(response.status_code, 200, msg=response.text)
+        host = 'test.host.net'
+        cluster_url = reverse('cluster')
+
+        self.load_bundle(self.bundle_adh_name)
+        self.load_bundle(self.bundle_ssh_name)
 
         adh_bundle_id, cluster_proto = self.get_cluster_proto_id()
-        response = self.api_post('/cluster/', {'name': self.cluster, 'prototype_id': cluster_proto})
+
+        response = self.client.post(
+            cluster_url, {'name': self.cluster, 'prototype_id': cluster_proto}
+        )
         cluster_id = response.json()['id']
+        this_cluster_host_url = reverse('host', kwargs={'cluster_id': cluster_id})
+
         ssh_bundle_id, _, host_id = self.create_host(self.host)
 
-        response = self.api_post('/cluster/' + str(cluster_id) + '/host/', {})
-        self.assertEqual(response.status_code, 400, msg=response.text)
+        response = self.client.post(this_cluster_host_url, {})
+        self.assertEqual(response.status_code, 400, msg=response.content)
         self.assertEqual(response.json()['host_id'], ['This field is required.'])
 
-        response = self.api_post('/cluster/' + str(cluster_id) + '/host/', {'host_id': 100500})
-        self.assertEqual(response.status_code, 404, msg=response.text)
+        response = self.client.post(this_cluster_host_url, {'host_id': 100500})
+        self.assertEqual(response.status_code, 404, msg=response.content)
         self.assertEqual(response.json()['code'], 'HOST_NOT_FOUND')
 
-        response = self.api_post('/cluster/' + str(cluster_id) + '/host/', {'host_id': host_id})
-        self.assertEqual(response.status_code, 201, msg=response.text)
+        response = self.client.post(this_cluster_host_url, {'host_id': host_id})
+        self.assertEqual(response.status_code, 201, msg=response.content)
         self.assertEqual(response.json()['id'], host_id)
         self.assertEqual(response.json()['cluster_id'], cluster_id)
 
-        response = self.api_post('/cluster/', {'name': 'qwe', 'prototype_id': cluster_proto})
+        response = self.client.post(cluster_url, {'name': 'qwe', 'prototype_id': cluster_proto})
         cluster_id2 = response.json()['id']
+        second_cluster_host_url = reverse('host', kwargs={'cluster_id': cluster_id2})
 
-        response = self.api_post('/cluster/' + str(cluster_id2) + '/host/', {'host_id': host_id})
-        self.assertEqual(response.status_code, 409, msg=response.text)
+        response = self.client.post(second_cluster_host_url, {'host_id': host_id})
+        self.assertEqual(response.status_code, 409, msg=response.content)
         self.assertEqual(response.json()['code'], 'FOREIGN_HOST')
 
-        response = self.api_post('/cluster/' + str(cluster_id) + '/host/', {'host_id': host_id})
-        self.assertEqual(response.status_code, 409, msg=response.text)
+        response = self.client.post(this_cluster_host_url, {'host_id': host_id})
+        self.assertEqual(response.status_code, 409, msg=response.content)
         self.assertEqual(response.json()['code'], 'HOST_CONFLICT')
 
-        response = self.api_delete('/cluster/' + str(cluster_id) + '/host/' + str(host_id) + '/')
-        self.assertEqual(response.status_code, 204, msg=response.text)
+        response = self.client.delete(this_cluster_host_url + str(host_id) + '/')
+        self.assertEqual(response.status_code, 204, msg=response.content)
 
-        response = self.api_post('/cluster/' + str(cluster_id2) + '/host/', {'host_id': host_id})
-        self.assertEqual(response.status_code, 201, msg=response.text)
+        response = self.client.post(second_cluster_host_url, {'host_id': host_id})
+        self.assertEqual(response.status_code, 201, msg=response.content)
         self.assertEqual(response.json()['cluster_id'], cluster_id2)
 
-        self.api_delete('/cluster/' + str(cluster_id) + '/')
-        self.api_delete('/cluster/' + str(cluster_id2) + '/')
-        self.api_delete('/host/' + str(host_id) + '/')
-        response = self.api_delete('/stack/bundle/' + str(adh_bundle_id) + '/')
-        self.assertEqual(response.status_code, 204, msg=response.text)
-        response = self.api_delete('/stack/bundle/' + str(ssh_bundle_id) + '/')
-        self.assertEqual(response.status_code, 204, msg=response.text)
+        self.client.delete(reverse('cluster-details', kwargs={'cluster_id': cluster_id}))
+        self.client.delete(reverse('cluster-details', kwargs={'cluster_id': cluster_id2}))
+        self.client.delete(reverse('host-details', kwargs={'host_id': host_id}))
+        response = self.client.delete(
+            reverse('bundle-details', kwargs={'bundle_id': adh_bundle_id})
+        )
+        self.assertEqual(response.status_code, 204, msg=response.content)
+        response = self.client.delete(
+            reverse('bundle-details', kwargs={'bundle_id': ssh_bundle_id})
+        )
+        self.assertEqual(response.status_code, 409, msg=response.content)
+        self.assertEqual(response.json()['code'], 'BUNDLE_CONFLICT')
 
     def test_service(self):
         response = self.api_post('/stack/load/', {'bundle_file': self.adh_bundle})
