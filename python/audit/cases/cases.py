e4d952cfd7b432ea3326ca21897c6d4c6906eb2e
Konstantin Shestakov <winmasta@yandex.ru>
2022-08-23 18:13:38 +0700

    ADCM-3099 add component config update audit

--- python/audit/cases/cases.py
+++ python/audit/cases/cases.py
@@ -1026,18 +1026,24 @@ def get_audit_operation_and_object(
                 object_type=AuditObjectType.Service,
             )
 
-        case ["component", component_pk, "config", "history", _, "restore"]:
+        case (
+            ["component", component_pk, "config", "history"]
+            | ["component", component_pk, "config", "history", _, "restore"]
+        ):
             audit_operation = AuditOperation(
                 name=f"{AuditObjectType.Component.capitalize()} "
                      f"configuration {AuditLogOperationType.Update}d",
                 operation_type=AuditLogOperationType.Update,
             )
-            obj = ServiceComponent.objects.get(pk=component_pk)
-            audit_object = get_or_create_audit_obj(
-                object_id=component_pk,
-                object_name=obj.name,
-                object_type=AuditObjectType.Component,
-            )
+            obj = ServiceComponent.objects.filter(pk=component_pk).first()
+            if obj:
+                audit_object = get_or_create_audit_obj(
+                    object_id=component_pk,
+                    object_name=obj.name,
+                    object_type=AuditObjectType.Component,
+                )
+            else:
+                audit_object = None
 
         case (
             ["adcm", adcm_pk, "config", "history"]
