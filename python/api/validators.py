f20b85f3defc5c0fab16512ed6dad1dc6949b997
dskrynnik <danbalalan@gmail.com>
2022-09-05 14:55:52 +0300

    ADCM-3119 refactor validator

--- python/api/validators.py
+++ python/api/validators.py
@@ -1,21 +1,12 @@
-import re
-
+from django.conf import settings
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.core.validators import RegexValidator
 
 from cm.errors import AdcmEx
 
-CLUSTER_NAME_PATTERN = re.compile(
-    r"^[a-zA-Z]"  # starts with latin letter (upper/lower case)
-    r"[a-zA-Z0-9\-\. ]*?"  # latin letters (upper/lower case), digits, hyphens, dots, whitespaces
-    r"[a-zA-Z0-9]$"  # ends with latin letter (upper/lower case) or digit
-)  # as a result of this pattern min_length = 2
-
 
 class RegExValidator(RegexValidator):
-    def __init__(
-        self, *args, regex: str | re.Pattern, error_code: str, error_msg: str = "", **kwargs
-    ):
+    def __init__(self, regex: str, *args, error_code: str, error_msg: str = "", **kwargs):
         super().__init__(regex, *args, **kwargs)
         self.error_code = error_code
         self.error_msg = error_msg
@@ -28,7 +19,7 @@ class RegExValidator(RegexValidator):
 
 
 ClusterNameRegExValidator = RegExValidator(
-    regex=CLUSTER_NAME_PATTERN,
+    regex=settings.CLUSTER_NAME_PATTERN,
     error_code="CLUSTER_CONFLICT",
     error_msg="Name `{value}` doesn't meets requirements",
 )
