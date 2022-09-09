906411949062c45906c498e3b851e51ac1f226ec
Daniil Skrynnik <danbalalan@gmail.com>
2022-09-09 19:07:20 +0300

    set encoding

--- python/utils.py
+++ python/utils.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 def write_to_wsgi_stdout(msg: str) -> None:
-    with open(settings.PID_FILE) as f:
+    with open(settings.PID_FILE, encoding="utf-8") as f:
         uwsgi_pid = f.read().strip()
     fd = os.open(f"/proc/{uwsgi_pid}/fd/1", os.O_WRONLY)
     if not msg.endswith(os.linesep):
