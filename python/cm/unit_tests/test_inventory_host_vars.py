8cdb2dce5b36b46ee72b0023524ef88391bf018b
Sofya Talavrinova <tsd@arenadata.io>
2022-09-12 16:37:01 +0300

    Fix linters

--- python/cm/unit_tests/test_inventory_host_vars.py
+++ python/cm/unit_tests/test_inventory_host_vars.py
@@ -1,5 +1,16 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from django.test import TestCase
-from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 
 from cm.unit_tests import utils
@@ -13,6 +24,7 @@ from cm.models import (
 
 
 class TestInventory(TestCase):
+    # pylint: disable=too-many-locals
     # TODO: refactor this file and merge it with cm/tests/test_inventory after audit merge
 
     def setUp(self):
