5ed5ac11c90aa72b403f5c7e979cc50886ea7648
dskrynnik <danbalalan@gmail.com>
2022-09-12 14:18:17 +0300

    ADCM-3180 rm file existence check

--- python/utils.py
+++ python/utils.py
@@ -1,19 +1,13 @@
 import os
-from pathlib import Path
 
 from django.conf import settings
 
 
-def write_to_uwsgi_stdout(msg: str) -> bool:
-    if not Path(settings.PID_FILE).is_file():
-        return False
+def write_to_uwsgi_stdout(msg: str) -> None:
     with open(settings.PID_FILE, encoding="utf-8") as f:
         uwsgi_pid = f.read().strip()
     fd_1 = f"/proc/{uwsgi_pid}/fd/1"
-    if not Path(fd_1).is_fifo():
-        return False
     fd = os.open(fd_1, os.O_WRONLY)
     if not msg.endswith(os.linesep):
         msg = f"{msg}{os.linesep}"
     os.write(fd, msg.encode("utf-8"))
-    return True
