6cb70e0372403d6670beda56c2b55aa7597db060
dskrynnik <danbalalan@gmail.com>
2022-09-12 10:14:57 +0300

    ADCM-3180 check file existence via Path

--- python/utils.py
+++ python/utils.py
@@ -1,15 +1,16 @@
 import os
+from pathlib import Path
 
 from django.conf import settings
 
 
 def write_to_uwsgi_stdout(msg: str) -> bool:
-    if not os.path.exists(settings.PID_FILE):
+    if not Path(settings.PID_FILE).is_file():
         return False
     with open(settings.PID_FILE, encoding="utf-8") as f:
         uwsgi_pid = f.read().strip()
     fd_1 = f"/proc/{uwsgi_pid}/fd/1"
-    if not os.path.exists(fd_1):
+    if not Path(fd_1).is_fifo():
         return False
     fd = os.open(fd_1, os.O_WRONLY)
     if not msg.endswith(os.linesep):
