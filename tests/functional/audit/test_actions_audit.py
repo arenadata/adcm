dce634a35c5041dcb225c21150ff037d3d23f647
Araslanov Egor <egaraslanoff@yandex.ru>
2022-09-22 15:02:22 +0500

    ADCM-3187 Tests on audit of objects update (cluster/host) (#2072)
    * ADCM-3187 Test on audit of objects' updates
    
    * ADCM-3187 Fix test
    
    * ADCM-3187 PATCH fixed

--- tests/functional/audit/test_actions_audit.py
+++ tests/functional/audit/test_actions_audit.py
@@ -18,7 +18,7 @@ import allure
 import pytest
 import requests
 from adcm_client.audit import AuditOperation, ObjectType, OperationResult, OperationType
-from adcm_client.objects import ADCM, ADCMClient, Bundle, Cluster, Job, Policy, Provider, Task
+from adcm_client.objects import ADCMClient, Bundle, Cluster, Job, Provider, Task
 from adcm_pytest_plugin.utils import wait_until_step_succeeds
 
 from tests.functional.audit.conftest import (
@@ -33,8 +33,7 @@ from tests.functional.audit.conftest import (
     parametrize_audit_scenario_parsing,
 )
 from tests.functional.rbac.conftest import BusinessRoles as BR
-from tests.functional.rbac.conftest import create_policy
-from tests.functional.tools import AnyADCMObject, ClusterRelatedObject, ProviderRelatedObject
+from tests.functional.tools import AnyADCMObject
 from tests.library.audit.checkers import AuditLogChecker
 
 # pylint: disable=redefined-outer-name
@@ -58,15 +57,6 @@ def provider(sdk_client_fs) -> Provider:
     provider = bundle.provider_create("Actions Provider")
     provider.host_create("host-fqdn")
     return provider
-
-
-@pytest.fixture()
-def build_policy(
-    sdk_client_fs, new_user_client
-) -> Callable[[BR, Union[ClusterRelatedObject, ProviderRelatedObject, ADCM]], Policy]:
-    """Prepare "policy builder" that grants some permission to (already created) new user"""
-    user_id = new_user_client.me().id
-    return lambda role, obj: create_policy(sdk_client_fs, role, [obj], [sdk_client_fs.user(id=user_id)], [])
 
 
 @pytest.fixture()
