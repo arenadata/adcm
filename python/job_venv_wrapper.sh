523b9cd1a953efa824fe5fea79b37910ae43a869
Konstantin Shestakov <winmasta@yandex.ru>
2022-10-03 19:11:10 +0700

    ADCM-3221 fixes

--- python/job_venv_wrapper.sh
+++ python/job_venv_wrapper.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Source virtualenv if it is required
 if [[ "${1}" != "none" ]]; then
     venv="/adcm/venv/${1}/bin/activate"
-    # shellcheck disable=SC1090
     . "${venv}"
 fi
 
 shift
 
-# run command
 "$@"
