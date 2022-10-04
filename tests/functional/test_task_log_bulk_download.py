141436bd4a4a9e9fd325847f217579a6fce04440
Sealwing <egaraslanoff@yandex.ru>
2022-10-04 14:39:34 +0500

    ADCM-3252 Test on task logs archive content

--- tests/functional/test_task_log_bulk_download.py
+++ tests/functional/test_task_log_bulk_download.py
@@ -22,6 +22,7 @@ from typing import Callable, Collection, Dict, List, NamedTuple, Set, Union
 import allure
 import pytest
 from adcm_client.objects import ADCM, ADCMClient, Cluster, Component, Host, Provider, Service, Task
+from adcm_pytest_plugin.docker_utils import ADCM as TestADCM
 from adcm_pytest_plugin.utils import get_data_dir
 from docker.models.containers import Container
 
@@ -193,6 +194,14 @@ def remove_archives(tmpdir: PathLike) -> None:
         archive.unlink()
 
 
+@allure.step("Remove all task log directories from FS")
+def remove_task_logs_from_fs(adcm_container: Container) -> None:
+    """Remove all task log directories from FS"""
+    exit_code, output = adcm_container.exec_run(["sh", "-c", "rm -r /adcm/data/run/*"])
+    if exit_code != 0:
+        raise RuntimeError(f"Failed to remove task log directories from FS: {output.decode('utf-8')}")
+
+
 # !===== Fixtures ======!
 
 
@@ -219,83 +228,139 @@ def _prepare_cluster_and_provider(cluster, provider) -> None:
 # !===== Tests ======!
 
 
-@pytest.mark.usefixtures("_prepare_cluster_and_provider")
-def test_naming(cluster, provider, adcm_fs, sdk_client_fs, tmpdir):
-    """Test naming of task's archive and its contents"""
-    _test_archiving_adcm_task(sdk_client_fs.adcm(), tmpdir)
-    service = cluster.service()
-    for adcm_object in (cluster, service, service.component(), provider):
-        _test_archiving_general_object_task(adcm_object, tmpdir)
-    _test_archiving_host_task(provider.host(), tmpdir)
-    remove_archives(tmpdir)
-    _test_archiving_from_db(sdk_client_fs, adcm_fs.container, tmpdir)
-    remove_archives(tmpdir)
-    _test_no_prototype(sdk_client_fs, tmpdir)
+class TestArchiveNaming:
+    """Test task logs archive naming"""
 
+    @pytest.mark.usefixtures("_prepare_cluster_and_provider")  # pylint: disable-next=too-many-arguments
+    def test_naming(self, cluster, provider, adcm_fs, sdk_client_fs, tmpdir):
+        """Test naming of task's archive and its contents"""
+        self._test_archiving_adcm_task(sdk_client_fs.adcm(), tmpdir)
+        service = cluster.service()
+        for adcm_object in (cluster, service, service.component(), provider):
+            self._test_archiving_general_object_task(adcm_object, tmpdir)
+        self._test_archiving_host_task(provider.host(), tmpdir)
+        remove_archives(tmpdir)
+        self._test_archiving_from_db(sdk_client_fs, adcm_fs.container, tmpdir)
+        remove_archives(tmpdir)
+        self._test_no_prototype(sdk_client_fs, tmpdir)
 
-@allure.step("Test ADCM's task archive naming")
-def _test_archiving_adcm_task(adcm: ADCM, tmpdir: PathLike) -> None:
-    clean_action_name = "test-ldap-connection"
-    adcm.config_set_diff(
-        {
-            "attr": {"ldap_integration": {"active": True}},
-            "config": {
-                "ldap_integration": {k: k for k in ("ldap_uri", "ldap_user", "ldap_password", "user_search_base")}
-            },
-        }
-    )
-    task = adcm.action(name=TEST_CONNECTION_ACTION).run()
-    task.wait()
-    with allure.step("Download task logs and check naming"):
-        archive = task.download_logs(tmpdir)
-        check_archive_name(archive, f"adcm_{clean_action_name}_{task.id}")
-        filenames = get_filenames_from_archive(archive)
-        check_job_directories(filenames, {clean_action_name})
-        check_all_files_presented_in_all_directories(filenames, {clean_action_name}, expected_files=FS_RUN_DIR_FILES_PY)
-
-
-def _test_archiving_general_object_task(
-    adcm_object: Union[Cluster, Service, Component, Provider], tmpdir: PathLike
-) -> None:
-    with allure.step(f"Test {adcm_object.__class__.__name__}'s task archive naming"):
-        for task in run_all_actions(adcm_object):
-            check_archive_naming(adcm_object, task, FS_RUN_DIR_FILES, build_full_archive_name, tmpdir)
-
-
-@allure.step("Test Host's task archive naming")
-def _test_archiving_host_task(host: Host, tmpdir: PathLike) -> None:
-    for task in run_all_actions(host):
-        check_archive_naming(host, task, FS_RUN_DIR_FILES, build_host_archive_name, tmpdir)
-
-
-def _test_archiving_from_db(client: ADCMClient, adcm_container: Container, tmpdir: PathLike) -> None:
-    with allure.step("Remove all task log directories from FS"):
-        exit_code, output = adcm_container.exec_run(["sh", "-c", "rm -r /adcm/data/run/*"])
-        if exit_code != 0:
-            raise RuntimeError(f"Failed to remove task log directories from FS: {output.decode('utf-8')}")
-    for adcm_object in (client.cluster(), client.service(), client.component(), client.provider()):
-        check_archive_naming(
-            adcm_object, _get_task_of(adcm_object, client), DB_RUN_DIR_FILES, build_full_archive_name, tmpdir
+    @allure.step("Test ADCM's task archive naming")
+    def _test_archiving_adcm_task(self, adcm: ADCM, tmpdir: PathLike) -> None:
+        clean_action_name = "test-ldap-connection"
+        adcm.config_set_diff(
+            {
+                "attr": {"ldap_integration": {"active": True}},
+                "config": {
+                    "ldap_integration": {k: k for k in ("ldap_uri", "ldap_user", "ldap_password", "user_search_base")}
+                },
+            }
         )
-    host = client.host()
-    check_archive_naming(host, _get_task_of(host, client), DB_RUN_DIR_FILES, build_host_archive_name, tmpdir)
-
-
-@allure.step("Test tasks without action's prototype")
-def _test_no_prototype(client: ADCMClient, tmpdir: PathLike) -> None:
-    objects = client.cluster(), client.service(), client.component(), client.provider(), client.host()
-    each_object_tasks: List[Task] = [_get_task_of(adcm_object, client) for adcm_object in objects]
-    with allure.step("Delete all bundles"):
-        client.host().delete()
-        client.provider().delete()
-        client.cluster().delete()
-        for bundle in client.bundle_list():
-            bundle.delete()
-    with allure.step("Check logs download of task with no action prototype"):
-        for task in each_object_tasks:
-            job_ids = {str(job.id) for job in task.job_list()}
-            archive: Path = task.download_logs(tmpdir)
-            check_archive_name(archive, str(task.id))
+        task = adcm.action(name=TEST_CONNECTION_ACTION).run()
+        task.wait()
+        with allure.step("Download task logs and check naming"):
+            archive = task.download_logs(tmpdir)
+            check_archive_name(archive, f"adcm_{clean_action_name}_{task.id}")
             filenames = get_filenames_from_archive(archive)
-            check_job_directories(filenames, job_ids, get_unique_directory_names)
-            check_all_files_presented_in_all_directories(filenames, job_ids, DB_RUN_DIR_FILES)
+            check_job_directories(filenames, {clean_action_name})
+            check_all_files_presented_in_all_directories(
+                filenames, {clean_action_name}, expected_files=FS_RUN_DIR_FILES_PY
+            )
+
+    def _test_archiving_general_object_task(
+        self, adcm_object: Union[Cluster, Service, Component, Provider], tmpdir: PathLike
+    ) -> None:
+        with allure.step(f"Test {adcm_object.__class__.__name__}'s task archive naming"):
+            for task in run_all_actions(adcm_object):
+                check_archive_naming(adcm_object, task, FS_RUN_DIR_FILES, build_full_archive_name, tmpdir)
+
+    @allure.step("Test Host's task archive naming")
+    def _test_archiving_host_task(self, host: Host, tmpdir: PathLike) -> None:
+        for task in run_all_actions(host):
+            check_archive_naming(host, task, FS_RUN_DIR_FILES, build_host_archive_name, tmpdir)
+
+    def _test_archiving_from_db(self, client: ADCMClient, adcm_container: Container, tmpdir: PathLike) -> None:
+        remove_task_logs_from_fs(adcm_container)
+        for adcm_object in (client.cluster(), client.service(), client.component(), client.provider()):
+            check_archive_naming(
+                adcm_object, _get_task_of(adcm_object, client), DB_RUN_DIR_FILES, build_full_archive_name, tmpdir
+            )
+        host = client.host()
+        check_archive_naming(host, _get_task_of(host, client), DB_RUN_DIR_FILES, build_host_archive_name, tmpdir)
+
+    @allure.step("Test tasks without action's prototype")
+    def _test_no_prototype(self, client: ADCMClient, tmpdir: PathLike) -> None:
+        objects = client.cluster(), client.service(), client.component(), client.provider(), client.host()
+        each_object_tasks: List[Task] = [_get_task_of(adcm_object, client) for adcm_object in objects]
+        with allure.step("Delete all bundles"):
+            client.host().delete()
+            client.provider().delete()
+            client.cluster().delete()
+            for bundle in client.bundle_list():
+                bundle.delete()
+        with allure.step("Check logs download of task with no action prototype"):
+            for task in each_object_tasks:
+                job_ids = {str(job.id) for job in task.job_list()}
+                archive: Path = task.download_logs(tmpdir)
+                check_archive_name(archive, str(task.id))
+                filenames = get_filenames_from_archive(archive)
+                check_job_directories(filenames, job_ids, get_unique_directory_names)
+                check_all_files_presented_in_all_directories(filenames, job_ids, DB_RUN_DIR_FILES)
+
+
+class TestArchiveContent:
+    """Test content of an archive"""
+
+    tmpdir: PathLike
+    adcm_fs: TestADCM
+
+    @pytest.fixture()
+    def _init(self, tmpdir, adcm_fs):
+        self.tmpdir = tmpdir
+        self.adcm_fs = adcm_fs
+
+    @pytest.mark.parametrize("cluster", ["content"], indirect=True)
+    @pytest.mark.usefixtures("_prepare_cluster_and_provider", "_init")
+    def test_content(self, cluster, adcm_fs, tmpdir):
+        """Test content of archived files: from FS and DB"""
+        task = self.run_component_action(cluster)
+        self.check_logs_from_fs(task)
+        remove_archives(tmpdir)
+        remove_task_logs_from_fs(adcm_fs.container)
+        self.check_logs_from_db(task)
+
+    def run_component_action(self, cluster: Cluster) -> Task:
+        """Run component action, wait until it's finished and return task"""
+        task = cluster.service().component().action().run()
+        task.wait()
+        return task
+
+    def check_logs_from_fs(self, task: Task) -> None:
+        """Check that logs in archive is the same as log files on FS"""
+        archive: Path = task.download_logs(self.tmpdir)
+        with tarfile.open(archive, "r") as tar:
+            for name_in_archive in tar.getnames():
+                with allure.step(f"Check that file {name_in_archive} in archive has the content from FS"):
+                    file_from_archive = tar.extractfile(name_in_archive).read().decode("utf-8")
+                    name = name_in_archive.split("/")[-1]
+                    exit_code, output = self.adcm_fs.container.exec_run(["cat", f"/adcm/data/run/{task.id}/{name}"])
+                    file_from_fs = output.decode("utf-8")
+                    if exit_code != 0:
+                        raise ValueError(f"docker exec failed: {file_from_fs}")
+                    if file_from_archive != file_from_fs:
+                        allure.attach(file_from_archive, "File from archive")
+                        allure.attach(file_from_fs, "File from FS")
+                        raise AssertionError("Incorrect file content")
+
+    def check_logs_from_db(self, task: Task) -> None:
+        """Check that logs in archive is the same as log records in DB"""
+        archive: Path = task.download_logs(self.tmpdir)
+        with tarfile.open(archive, "r") as tar:
+            for log_type in ("stdout", "stderr"):
+                with allure.step(f"Check archived log from DB of type {log_type}"):
+                    filename = next(name for name in tar.getnames() if log_type in name)
+                    file_from_archive = tar.extractfile(filename).read().decode("utf-8")
+                    log_from_db = task.job().log(type=log_type).content
+                    if file_from_archive != log_from_db:
+                        allure.attach(file_from_archive, "File from archive")
+                        allure.attach(log_from_db, "Log from DB")
+                        raise AssertionError("Incorrect file content")
