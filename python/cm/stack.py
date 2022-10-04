305b57903cef6c9d55185cb25123290ecbc52828
Sofya Talavrinova <tsd@arenadata.io>
2022-10-04 11:03:23 +0300

    ADCM-3254 delete check for too long name on bundle uploading, bump version

--- python/cm/stack.py
+++ python/cm/stack.py
@@ -47,7 +47,6 @@ from cm.models import (
 )
 
 NAME_REGEX = r"[0-9a-zA-Z_\.-]+"
-MAX_NAME_LENGTH = 256
 
 
 def save_definition(path, fname, conf, obj_list, bundle_hash, adcm=False):
@@ -673,8 +672,6 @@ def validate_name(value, name):
     )
     if p.fullmatch(value) is None:
         err("WRONG_NAME", msg1.format(name))
-    if len(value) > MAX_NAME_LENGTH:
-        err("LONG_NAME", f"{name} is too long. Max length is {MAX_NAME_LENGTH}")
     return value
 
 
