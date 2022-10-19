4325f7a03767044fc1e93cbb85655a52a80e7319
dskrynnik <danbalalan@gmail.com>
2022-10-19 14:46:52 +0300

    ADCM-3095 refactor dict_json_get_or_create

--- python/cm/utils.py
+++ python/cm/utils.py
@@ -2,11 +2,11 @@ import json
 from typing import Any
 
 
-def dict_json_get_or_create(path, field, value=None) -> Any:
+def dict_json_get_or_create(path: str, field: str, value: Any = None) -> Any:
     with open(path, encoding="utf-8") as f:
         data = json.load(f)
 
-    if data.get(field) is None:
+    if field not in data:
         data[field] = value
         with open(path, "w", encoding="utf-8") as f:
             json.dump(data, f)
