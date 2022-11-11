2f4da854df84a33adeae458f5f1d888fbb9cb42e
Sealwing <egaraslanoff@yandex.ru>
2022-11-11 12:05:05 +0500

    ADCM-3312 Added test on MM flag in inventory

--- tests/functional/maintenance_mode/test_inventory_modification.py
+++ tests/functional/maintenance_mode/test_inventory_modification.py
@@ -19,21 +19,29 @@ from typing import Set, Tuple
 
 import allure
 import pytest
-from adcm_client.objects import Action, Cluster, Component, GroupConfig, Host
+from adcm_client.objects import Action, ADCMClient, Cluster, Component, GroupConfig, Host, Service
 from adcm_pytest_plugin.docker_utils import ADCM
 from adcm_pytest_plugin.utils import get_or_add_service
 
 from tests.functional.conftest import only_clean_adcm
 from tests.functional.maintenance_mode.conftest import (
+    BUNDLES_DIR,
     DEFAULT_SERVICE_NAME,
     FIRST_COMPONENT,
+    MM_IS_OFF,
     MM_IS_ON,
     SECOND_COMPONENT,
     add_hosts_to_cluster,
+    set_maintenance_mode,
     turn_mm_on,
 )
 from tests.functional.maintenance_mode.test_hosts_behavior import ACTION_ALLOWED_IN_MM
-from tests.functional.tools import build_hc_for_hc_acl_action, create_config_group_and_add_host, get_inventory_file
+from tests.functional.tools import (
+    build_hc_for_hc_acl_action,
+    create_config_group_and_add_host,
+    get_inventory_file,
+    get_object_represent,
+)
 from tests.library.assertions import sets_are_equal
 
 # pylint: disable=redefined-outer-name
@@ -42,6 +50,8 @@ pytestmark = [only_clean_adcm]
 
 DEFAULT_ACTION_NAME = "default_action"
 HC_ACL_SERVICE_NAME = "hc_acl_service"
+
+DEFAULT_SERVICE_COMPONENT_MM_BUNDLE = "cluster_mm_allowed"
 
 
 @pytest.fixture(params=[pytest.param(DEFAULT_SERVICE_NAME, id="default_service")])
@@ -63,6 +73,20 @@ def cluster_with_hc_set(request, cluster_with_mm, hosts) -> Cluster:
         *[(host, second_component) for host in hosts_in_cluster[:2]],
     )
     return cluster_with_mm
+
+
+@pytest.fixture(params=[pytest.param(DEFAULT_SERVICE_COMPONENT_MM_BUNDLE, id="default_cluster")])
+def cluster_with_service_component_mm(
+    request, sdk_client_fs, hosts
+) -> tuple[Cluster, Service, Component, Component, Host, Host]:
+    bundle = sdk_client_fs.upload_from_fs(BUNDLES_DIR / request.param)
+    cluster = bundle.cluster_create("Cluster with allowed MM")
+    service = cluster.service_add(name="test_service")
+    first_component = service.component(name="first_component")
+    second_component = service.component(name="second_component")
+    host_1, host_2, *_ = hosts
+    cluster.hostcomponent_set((cluster.host_add(host_1), first_component), (cluster.host_add(host_2), second_component))
+    return cluster, service, first_component, second_component, host_1, host_2
 
 
 @pytest.fixture()
@@ -158,6 +182,37 @@ def test_host_filtering_with_hc_acl(api_client, adcm_fs, cluster_with_hc_set: Cl
     _check_add_remove_groups(inventory, add={free_host.fqdn}, remove=set())
 
 
+def test_mm_flag_on_service_and_components(cluster_with_service_component_mm, sdk_client_fs, api_client, adcm_fs: ADCM):
+    """Test that MM flag is set correctly on services and components in inventory"""
+    action_name = "allowed_in_mm"
+    _, service, first_component, second_component, _, second_host = cluster_with_service_component_mm
+
+    set_maintenance_mode(api_client=api_client, adcm_object=service, maintenance_mode=MM_IS_ON)
+    check_mm_flag_in_inventory(
+        client=sdk_client_fs,
+        inventory=run_action_and_get_inventory(service.action(name=action_name), adcm_fs),
+        expect_on=(service, first_component, second_component),
+    )
+
+    set_maintenance_mode(api_client=api_client, adcm_object=service, maintenance_mode=MM_IS_OFF)
+    set_maintenance_mode(api_client=api_client, adcm_object=first_component, maintenance_mode=MM_IS_ON)
+    check_mm_flag_in_inventory(
+        client=sdk_client_fs,
+        inventory=run_action_and_get_inventory(service.action(name=action_name), adcm_fs),
+        expect_on=(first_component,),
+        expect_off=(service, second_component),
+    )
+
+    set_maintenance_mode(api_client=api_client, adcm_object=second_host, maintenance_mode=MM_IS_ON)
+    set_maintenance_mode(api_client=api_client, adcm_object=first_component, maintenance_mode=MM_IS_OFF)
+    check_mm_flag_in_inventory(
+        client=sdk_client_fs,
+        inventory=run_action_and_get_inventory(service.action(name=action_name), adcm_fs),
+        expect_on=(second_component,),
+        expect_off=(service, first_component),
+    )
+
+
 def run_action_and_get_inventory(action: Action, adcm: ADCM, **run_kwargs) -> dict:
     """Run action and get inventory file contents from container"""
     with allure.step(f"Run action {action.name}"):
@@ -189,6 +244,28 @@ def check_hosts_in_mm_are_absent(inventory: dict, cluster: Cluster, service_name
         raise RuntimeError('There should be at least 1 component with host in MM')
     for component in components_with_mm:
         _check_mm_groups(inventory, service_name, component, hostnames_in_mm)
+
+
+@allure.step("Check MM attributes of services and components in inventory")
+def check_mm_flag_in_inventory(
+    client: ADCMClient,
+    inventory: dict,
+    expect_on: tuple[Service | Component, ...] = (),
+    expect_off: tuple[Service | Component, ...] = (),
+):
+    # pylint: disable=unsupported-membership-test,unsubscriptable-object
+    for obj in expect_on:
+        with allure.step(f"Check that MM of {get_object_represent(obj)} is 'ON' in inventory"):
+            node = _get_object_node(inventory=inventory, client=client, adcm_object=obj)
+            assert "maintenance_mode" in node, f"No field 'maintenance_mode' found within {node.keys()}"
+            assert node["maintenance_mode"] is True
+
+    for obj in expect_off:
+        with allure.step(f"Check that MM of {get_object_represent(obj)} is 'OFF' in inventory"):
+            node = _get_object_node(inventory=inventory, client=client, adcm_object=obj)
+            assert "maintenance_mode" in node, f"No field 'maintenance_mode' found within {node.keys()}"
+            assert node["maintenance_mode"] is False
+    # pylint: enable=unsupported-membership-test,unsubscriptable-object
 
 
 @allure.step("Check that correct hosts are presented in regular groups")
@@ -247,3 +324,14 @@ def _check_add_remove_groups(inventory, add: Set[str], remove: Set[str]):
     with allure.step(f"Check node {node_name}"):
         actual_hosts = set(node_value["hosts"].keys())
         sets_are_equal(actual_hosts, remove, '"remove" node is incorrect')
+
+
+def _get_object_node(inventory: dict, client: ADCMClient, adcm_object: Service | Component) -> dict:
+    services_node = inventory["all"]["children"]["CLUSTER"]["vars"]["services"]
+    match adcm_object:
+        case Service():
+            return services_node[adcm_object.name]
+        case Component():
+            return services_node[client.service(id=adcm_object.service_id).name][adcm_object.name]
+        case _:
+            raise ValueError("`adcm_object` can be only Service or Component")
