7b6cd5e3a78b0a5715d3f5ad816d61db086652b3
dbardin90 <d.bardin@arenadata.io>
2023-01-18 10:10:26 +0300

    ADCM-3343 - [UI] Add support of secretfile type on Configuration/Configuration Group page

---
+++ web/src/app/shared/form-elements/secret-file/secret-file.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { SecretTextComponent } from "@app/shared/form-elements/secret-text/secret-text.component";
+
+@Component({
+  selector: 'app-secret-file',
+  templateUrl: './secret-file.component.html',
+  styleUrls: ['./secret-file.component.scss']
+})
+export class SecretFileComponent extends SecretTextComponent implements OnInit {
+
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
