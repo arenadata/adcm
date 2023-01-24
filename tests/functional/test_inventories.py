44c0ce0274a8efa544abdfcb1069a5c85a982813
Vasiliy Chudasov <v.chudasov@arenadata.io>
2023-01-24 13:32:26 +0000

    ADCM-3507 add tests for inventory with more 1 hosts and 2 services with 2 components

--- tests/functional/test_inventories.py
+++ tests/functional/test_inventories.py
@@ -14,7 +14,6 @@
 
 import json
 from typing import Optional, Tuple
-from uuid import uuid4
 
 import allure
 import pytest
@@ -29,93 +28,220 @@ from adcm_client.objects import (
     Service,
 )
 from adcm_pytest_plugin import utils
-from adcm_pytest_plugin.docker_utils import get_file_from_container
-from adcm_pytest_plugin.steps.actions import run_cluster_action_and_assert_result
+from adcm_pytest_plugin.steps.actions import (
+    run_cluster_action_and_assert_result,
+    run_component_action_and_assert_result,
+)
 from adcm_pytest_plugin.utils import get_data_dir
 from docker.models.containers import Container
 from tests.functional.tools import (
     BEFORE_UPGRADE_DEFAULT_STATE,
+    compare_inventory_files,
     create_config_group_and_add_host,
     get_inventory_file,
 )
 
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name,too-many-locals
 
 
-HOST_FQDN = 'awesome-future'
-CLUSTER_NAME = 'dumMxpQA'
-GROUP_COMPONENT_NAME = 'GROUP_BUDDY'
-
+HOST_FQDN = "awesome-future"
+CLUSTER_NAME = "dumMxpQA"
+GROUP_COMPONENT_NAME = "GROUP_BUDDY"
+CHANGE_FLOAT = 3.0
+CLUSTER_ACTION_NAME = "cluster_action"
 CONFIG_TO_CHANGE = {
-    'config': {
-        'json_field': {'there_is': 'no one'},
-        'map_field': {'here': 'but me'},
-        '__main_info': "I'm a very important string, don't touch me",
-        'simple_field': 1,
+    "config": {
+        "json_field": {"there_is": "no one"},
+        "map_field": {"here": "but me"},
+        "__main_info": "I'm a very important string, don't touch me",
+        "simple_field": 1,
     },
-    'attr': {
-        'group_keys': {
-            'json_field': True,
-            'map_field': True,
-            'simple_field': False,
-            '__main_info': False,
+    "attr": {
+        "group_keys": {
+            "json_field": True,
+            "map_field": True,
+            "simple_field": False,
+            "__main_info": False,
         },
-        'custom_group_keys': {
-            'json_field': True,
-            'map_field': True,
-            'simple_field': True,
-            '__main_info': False,
+        "custom_group_keys": {
+            "json_field": True,
+            "map_field": True,
+            "simple_field": True,
+            "__main_info": False,
         },
     },
 }
 
 
-@allure.title('Create provider')
+@allure.title("Create provider")
 @pytest.fixture()
 def provider(sdk_client_fs: ADCMClient) -> Provider:
     """Get dummy provider"""
-    bundle = sdk_client_fs.upload_from_fs(utils.get_data_dir(__file__, 'provider'))
-    return bundle.provider_create(name='test_provider')
+    bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__, "provider"))
+    return bundle.provider_create(name="test_provider")
 
 
-@allure.title('Create cluster')
+@allure.title("Create cluster")
 @pytest.fixture()
 def cluster(sdk_client_fs: ADCMClient) -> Cluster:
     """Get cluster for inventory check"""
-    cluster_bundle = sdk_client_fs.upload_from_fs(utils.get_data_dir(__file__, 'cluster_inventory_tests'))
+    cluster_bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__, "cluster_inventory_tests"))
     return cluster_bundle.cluster_prototype().cluster_create(CLUSTER_NAME)
+
+
+@allure.title("Create cluster services and components")
+@pytest.fixture()
+def cluster_services_components(
+    sdk_client_fs: ADCMClient,
+) -> tuple[Cluster, Service, Service, Component, Component, Component, Component]:
+    """Get cluster for inventory check"""
+    cluster_bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__, "check_bundle"))
+    cluster = cluster_bundle.cluster_prototype().cluster_create("config_group")
+    service_1 = cluster.service_add(name="service_1")
+    service_2 = cluster.service_add(name="service_2")
+    component1_s1 = service_1.component(name="component_1")
+    component2_s1 = service_1.component(name="component_2")
+    component1_s2 = service_2.component(name="component_1")
+    component2_s2 = service_2.component(name="component_2")
+    return cluster, service_1, service_2, component1_s1, component2_s1, component1_s2, component2_s2
+
+
+def test_two_services_with_components_config_inventory(
+    cluster_services_components, provider: Provider, adcm_fs: ADCM, request: SubRequest
+):
+    """Assert inventory file contents for the cluster with two services and four components"""
+    adcm_objects = list(cluster_services_components)
+    host = provider.host_create(fqdn=HOST_FQDN)
+    (
+        cluster,
+        _,
+        _,
+        component1_s1,
+        component2_s1,
+        component1_s2,
+        component2_s2,
+    ) = cluster_services_components
+    cluster.host_add(host)
+
+    with allure.step("Configure cluster"):
+        cluster.hostcomponent_set(
+            (host, component1_s1), (host, component2_s1), (host, component1_s2), (host, component2_s2)
+        )
+
+    with allure.step("Create config group and set config"):
+        for obj in adcm_objects:
+            obj_float_name = (
+                f"{obj.__class__.__name__.lower()}_some_float" if obj == cluster else f"{obj.display_name}_some_float"
+            )
+            config_group = create_config_group_and_add_host("config group", obj, host)
+            config_group.config_set(
+                {"config": {obj_float_name: CHANGE_FLOAT}, "attr": {"group_keys": {obj_float_name: True}}}
+            )
+
+    with allure.step("Run action on component object"):
+        run_component_action_and_assert_result(component1_s1, CLUSTER_ACTION_NAME)
+        path_to_expected_inventory = get_data_dir(__file__, "check_bundle", "cluster-inventory.json")
+        compare_inventory_files(adcm=adcm_fs, path_to_expected=path_to_expected_inventory, job_id=1, request=request)
+
+
+def test_two_services_with_components_config_inventory_without_group(
+    cluster_services_components, provider: Provider, adcm_fs: ADCM, request: SubRequest
+):
+    """Assert inventory file contents for the cluster with two services and four components"""
+    adcm_objects = list(cluster_services_components)
+    host = provider.host_create(fqdn=HOST_FQDN)
+    (
+        cluster,
+        _,
+        _,
+        component1_s1,
+        component2_s1,
+        component1_s2,
+        component2_s2,
+    ) = cluster_services_components
+    cluster.host_add(host)
+
+    with allure.step("Configure cluster"):
+        cluster.hostcomponent_set(
+            (host, component1_s1), (host, component2_s1), (host, component1_s2), (host, component2_s2)
+        )
+
+    with allure.step("Create config group and set config"):
+        for obj in adcm_objects[:-1]:
+            obj_float_name = (
+                f"{obj.__class__.__name__.lower()}_some_float" if obj == cluster else f"{obj.display_name}_some_float"
+            )
+            config_group = create_config_group_and_add_host("config group", obj, host)
+            config_group.config_set(
+                {"config": {obj_float_name: CHANGE_FLOAT}, "attr": {"group_keys": {obj_float_name: True}}}
+            )
+
+    with allure.step("Run action on component object"):
+        run_component_action_and_assert_result(component1_s1, CLUSTER_ACTION_NAME)
+        path_to_expected_inventory = get_data_dir(
+            __file__, "check_bundle", "cluster-inventory-component-groupless.json"
+        )
+        compare_inventory_files(adcm=adcm_fs, path_to_expected=path_to_expected_inventory, job_id=1, request=request)
+
+
+def test_three_hosts_config_inventory(
+    cluster_services_components, provider: Provider, adcm_fs: ADCM, request: SubRequest
+):
+    host_2 = provider.host_create(fqdn="service-host")
+    host_3 = provider.host_create(fqdn="component-host")
+    (
+        cluster,
+        service_1,
+        service_2,
+        component1_s1,
+        component2_s1,
+        component1_s2,
+        component2_s2,
+    ) = cluster_services_components
+    cluster.host_add(host_2)
+    cluster.host_add(host_3)
+
+    with allure.step("Configure cluster"):
+        cluster.hostcomponent_set(
+            (host_2, component1_s1), (host_3, component2_s1), (host_2, component1_s2), (host_3, component2_s2)
+        )
+
+    with allure.step("Create config group and set config"):
+        cluster_group = cluster.group_config_create(name="config group")
+        cluster_group.host_add(host_2)
+        cluster_group.host_add(host_3)
+        cluster_group.config_set(
+            {"config": {"cluster_some_float": CHANGE_FLOAT}, "attr": {"group_keys": {"cluster_some_float": True}}}
+        )
+        _create_group_add_host_set_config(adcm_object=service_1, host=host_2)
+        _create_group_add_host_set_config(adcm_object=service_2, host=host_3)
+        _create_group_add_host_set_config(adcm_object=component1_s1, host=host_2)
+        _create_group_add_host_set_config(adcm_object=component2_s1, host=host_3)
+        _create_group_add_host_set_config(adcm_object=component1_s2, host=host_2)
+        _create_group_add_host_set_config(adcm_object=component2_s2, host=host_3)
+
+    with allure.step("Run action and check inventory"):
+        run_cluster_action_and_assert_result(cluster, "cluster_action")
+        path_to_expected_inventory = get_data_dir(__file__, "check_bundle", "cluster-inventory-hosts.json")
+        compare_inventory_files(adcm=adcm_fs, path_to_expected=path_to_expected_inventory, job_id=1, request=request)
 
 
 def test_check_inventories_file(cluster: Cluster, provider: Provider, adcm_fs: ADCM, request: SubRequest):
     """Assert inventory file contents for the action"""
     host = provider.host_create(fqdn=HOST_FQDN)
-    with allure.step('Configure cluster'):
+    with allure.step("Configure cluster"):
         service = cluster.service_add(name="zookeeper")
         cluster.host_add(host)
         component = service.component(name=GROUP_COMPONENT_NAME)
         cluster.hostcomponent_set((host, component))
-    with allure.step(f'Create config group with {GROUP_COMPONENT_NAME} and change it'):
-        config_group = create_config_group_and_add_host('group_name', component, host)
+    with allure.step(f"Create config group with {GROUP_COMPONENT_NAME} and change it"):
+        config_group = create_config_group_and_add_host("group_name", component, host)
         config_group.config_set(CONFIG_TO_CHANGE)
-    with allure.step('Run actions on cluster'):
+    with allure.step("Run actions on cluster"):
         run_cluster_action_and_assert_result(cluster, "set_multi_states")
         run_cluster_action_and_assert_result(cluster, "install")
-    with allure.step('Get inventory file from container'):
-        text = get_file_from_container(adcm_fs, '/adcm/data/run/2/', 'inventory.json')
-        inventory_content = text.read().decode('utf8')
-        inventory = json.loads(inventory_content)
-        _attach_inventory_file(request, inventory_content, 'Actual content of inventory.json')
-    with allure.step('Get expected inventory file'):
-        with open(utils.get_data_dir(__file__, 'cluster-inventory.json'), 'rb') as template:
-            expected_content = template.read().decode('utf8')
-            expected = json.loads(expected_content)
-        _attach_inventory_file(request, expected_content, 'Expected content of inventory.json')
-    with allure.step('Compare actual and expected config'):
-        assert (
-            inventory == expected
-        ), "Content of file inventory.json doesn't match expected. See attachments for more info."
-    with allure.step('Check that object attributes are available in ansible script'):
-        run_cluster_action_and_assert_result(cluster, 'check')
+        path_to_expected_inventory = get_data_dir(__file__, "cluster-inventory.json")
+        compare_inventory_files(adcm=adcm_fs, path_to_expected=path_to_expected_inventory, job_id=2, request=request)
 
 
 class TestStateBeforeUpgrade:
@@ -124,17 +250,17 @@ class TestStateBeforeUpgrade:
     @pytest.fixture()
     def old_cluster(self, sdk_client_fs) -> Cluster:
         """Upload old and new version of cluster, create old one"""
-        old_bundle = sdk_client_fs.upload_from_fs(utils.get_data_dir(__file__, "upgrade", "old"))
-        sdk_client_fs.upload_from_fs(utils.get_data_dir(__file__, "upgrade", "new"))
+        old_bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__, "upgrade", "old"))
+        sdk_client_fs.upload_from_fs(get_data_dir(__file__, "upgrade", "new"))
         return old_bundle.cluster_create(name="Test Cluster")
 
     def test_before_upgrade_in_inventory(self, adcm_fs, old_cluster):
         """Test value of before upgrade state in inventory.json before and after upgrade"""
-        with allure.step('Check before_upgrade state before upgrade'):
+        with allure.step("Check before_upgrade state before upgrade"):
             task = old_cluster.action(name="do_nothing").run()
             task.wait()
             self.check_before_upgrade_state_equal_to(BEFORE_UPGRADE_DEFAULT_STATE, get_inventory_file(adcm_fs, task.id))
-        with allure.step('Check before_upgrade state after upgrade'):
+        with allure.step("Check before_upgrade state after upgrade"):
             state_before_upgrade = old_cluster.state
             old_cluster.upgrade().do()
             old_cluster.reread()
@@ -215,16 +341,12 @@ def _read_job_inventory(container: Container, job_id: int) -> dict:
     return json.loads(content)
 
 
-def _attach_inventory_file(request: SubRequest, inventory_content: str, name: str):
-    """Attach inventory file on top level of allure report"""
-    reporter = utils.allure_reporter(request.config)
-    if not reporter:
-        return
-    test_result = reporter.get_test(uuid=None)
-    reporter.attach_data(
-        uuid=uuid4(),
-        body=inventory_content,
-        name=name,
-        attachment_type=allure.attachment_type.JSON,
-        parent_uuid=test_result.uuid,
+def _create_group_add_host_set_config(adcm_object: Service | Component, host: Host):
+    adcm_obj_group = adcm_object.group_config_create(name="config group")
+    adcm_obj_group.host_add(host)
+    adcm_obj_group.config_set(
+        {
+            "config": {f"{adcm_object.display_name}_some_float": CHANGE_FLOAT},
+            "attr": {"group_keys": {f"{adcm_object.display_name}_some_float": True}},
+        }
     )
