0f2024c39dbbbf065b4ce7c806a5decb96d4a336
Daniil Skrynnik <d.skrynnik@arenadata.io>
2023-01-24 13:30:23 +0000

    ADCM-3474 refactor tests

--- python/cm/tests/test_bundle.py
+++ python/cm/tests/test_bundle.py
@@ -11,15 +11,12 @@
 # limitations under the License.
 
 import json
-import os
-from contextlib import contextmanager
 from pathlib import Path
-from tarfile import TarFile
 
 from django.conf import settings
-from django.db import transaction
+from django.db import IntegrityError
+from django.db.transaction import TransactionManagementError
 from django.urls import reverse
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 
@@ -28,7 +25,7 @@ from cm.adcm_config import ansible_decrypt
 from cm.api import delete_host_provider
 from cm.bundle import delete_bundle
 from cm.errors import AdcmEx
-from cm.models import Bundle, ConfigLog
+from cm.models import ConfigLog
 from cm.tests.test_upgrade import (
     cook_cluster,
     cook_cluster_bundle,
@@ -38,157 +35,39 @@ from cm.tests.test_upgrade import (
 
 
 class TestBundle(BaseTestCase):
-    bundle_config_template = """
-- type: cluster
-  name: Monitoring
-  version: 666
-  edition: community
-  description: Monitoring and Control Software
-
-  upgrade:
-    - name: {upg1_name}
-      description: test upg1 description
-      from_edition:{upg1_from_edition}
-      versions:
-        min: "{upg1_min_version}"
-        max_strict: "{upg1_max_strict}"
-      scripts:
-        - name: {upg1_script1_name}
-          script: monitoring/bundle_pre_check.yaml
-          script_type: ansible
-          on_fail: running
-        - name: {upg1_script2_name}
-          script: bundle_switch
-          script_type: internal
-        - name: {upg1_script3_name}
-          script: monitoring/bundle_post_upgrade.yaml
-          params:
-            ansible_tags: install
-          script_type: ansible
-      states:
-        available: {upg1_state_available}
-        on_success: {upg1_state_on_success}
-    - name: {upg2_name}
-      from_edition:{upg2_from_edition}
-      versions:
-        min: "{upg2_min_version}"
-        max_strict: "{upg2_max_strict}"
-      scripts:
-        - name: {upg2_script1_name}
-          script: monitoring/bundle_pre_check.yaml
-          script_type: ansible
-          on_fail: running
-        - name: {upg2_script2_name}
-          script: bundle_switch
-          script_type: internal
-        - name: {upg2_script3_name}
-          script: monitoring/bundle_post_upgrade.yaml
-          params:
-            ansible_tags: install
-          script_type: ansible
-      states:
-        available: {upg2_state_available}
-        on_success: {upg2_state_on_success}
-"""
-
     def setUp(self) -> None:
         super().setUp()
+        self.files_dir = settings.BASE_DIR / "python" / "cm" / "tests" / "files"
 
-        self.files_dir = os.path.join(settings.BASE_DIR, "python", "cm", "tests", "files")
-        os.makedirs(self.files_dir, exist_ok=True)
-        self.tar_write_cfg = {}
+    def test_bundle_upload_duplicate_upgrade_fail(self):
+        with self.assertRaises(TransactionManagementError) as raises_context:
+            self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_duplicated.tar"))
 
-    @contextmanager
-    def make_bundle_from_str(self, bundle_content: str, filename: str) -> str:
-        tmp_filepath = os.path.join(self.files_dir, "config.yaml")
-        with open(tmp_filepath, "wt", encoding=settings.ENCODING_UTF_8) as config:
-            config.write(bundle_content)
+        # we expect here IntegrityError, but unittest do not raise it directly,
+        # so check context of TransactionManagementError
 
-        bundle_filepath = os.path.join(self.files_dir, filename)
-        with TarFile.open(
-            name=bundle_filepath,
-            mode="w",
-            encoding=settings.ENCODING_UTF_8,
-        ) as tar:
-            tar.add(name=tmp_filepath, arcname=os.path.basename(tmp_filepath))
-        os.remove(tmp_filepath)
+        actual_exception = raises_context.exception.__context__
+        self.assertIsNotNone(actual_exception)
+        self.assertIsInstance(actual_exception, IntegrityError)
+        self.assertIn("UNIQUE constraint failed", str(actual_exception))
 
-        try:
-            yield filename
-        finally:
-            os.remove(bundle_filepath)
+    def test_bundle_upload_upgrade_different_upgrade_name_success(self):
+        self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_different_name.tar"))
 
-    def _load_bundle(self, bundle_name: str) -> int:
-        with open(Path(self.files_dir, bundle_name), encoding=settings.ENCODING_UTF_8) as f:
-            with transaction.atomic():
-                response = self.client.post(
-                    path=reverse("upload-bundle"),
-                    data={"file": f},
-                )
-            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        with transaction.atomic():
-            response = self.client.post(
-                path=reverse("load-bundle"),
-                data={"bundle_file": bundle_name},
-            )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        return response.json()["id"]
+    def test_bundle_upload_upgrade_different_from_edition_success(self):
+        self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_different_from_edition.tar"))
 
-    def test_upload_duplicated_upgrade_script_names(self):
-        same_upgrade_name = "Upgrade name"
-        same_script_name = "Script name"
-        same_version = "2.11"
-        same_state_available = "any"
-        same_state_on_success = "upgradable"
-        same_from_edition = "\n        - community\n        - enterprise"
-        kwargs = {
-            "upg1_name": same_upgrade_name,
-            "upg1_from_edition": same_from_edition,
-            "upg1_min_version": same_version,
-            "upg1_max_strict": same_version,
-            "upg1_script1_name": same_script_name,
-            "upg1_script2_name": same_script_name,
-            "upg1_script3_name": same_script_name,
-            "upg1_state_available": same_state_available,
-            "upg1_state_on_success": same_state_on_success,
-            "upg2_name": same_upgrade_name,
-            "upg2_from_edition": same_from_edition,
-            "upg2_min_version": same_version,
-            "upg2_max_strict": same_version,
-            "upg2_script1_name": same_script_name,
-            "upg2_script2_name": same_script_name,
-            "upg2_script3_name": same_script_name,
-            "upg2_state_available": same_state_available,
-            "upg2_state_on_success": same_state_on_success,
-        }
-        with self.make_bundle_from_str(
-            bundle_content=self.bundle_config_template.format(**kwargs),
-            filename="test_bundle.tar.gz",
-        ) as bundle:
-            try:
-                bundle_id = self._load_bundle(bundle)
-                Bundle.objects.get(pk=bundle_id).delete()
-            except transaction.TransactionManagementError:  # == IntegrityError
-                pass
-            else:
-                raise AssertionError("Same upgrades should not be allowed to uploaded")
+    def test_bundle_upload_upgrade_different_min_version_success(self):
+        self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_different_min_version.tar"))
 
-        # if at least one of these values is different, it is considered to be a different upgrade
-        different_values = [
-            {"upg1_name": "New name"},
-            {"upg1_from_edition": "\n        - community"},
-            {"upg1_min_version": "0.1"},
-            {"upg1_max_strict": "3.9"},
-            {"upg1_state_available": "[running]"},
-            {"upg1_state_on_success": "new shiny state on success"},
-        ]
-        for value in different_values:
-            with self.make_bundle_from_str(
-                bundle_content=self.bundle_config_template.format(**{**kwargs, **value}),
-                filename="test_bundle.tar.gz",
-            ) as bundle:
-                bundle_id = self._load_bundle(bundle)
-                Bundle.objects.get(pk=bundle_id).delete()
+    def test_bundle_upload_upgrade_different_max_strict_success(self):
+        self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_different_max_strict.tar"))
+
+    def test_bundle_upload_upgrade_different_state_available_success(self):
+        self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_different_state_available.tar"))
+
+    def test_bundle_upload_upgrade_different_state_on_success_success(self):
+        self.upload_and_load_bundle(path=Path(self.files_dir, "test_upgrade_different_state_on_success.tar"))
 
     def test_secretfile(self):
         bundle, cluster, config_log = self.upload_bundle_create_cluster_config_log(
