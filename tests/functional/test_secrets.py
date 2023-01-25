a3c3fdc127ce5c6377539c829200633c4b321d0a
Vasiliy Chudasov <v.chudasov@arenadata.io>
2023-01-25 12:20:25 +0000

    ADCM-3382 add test secret map. Add new test test_secret_map for ui. Add checks secret_map to test test_secrets

--- tests/functional/test_secrets.py
+++ tests/functional/test_secrets.py
@@ -28,9 +28,18 @@ OLD_PASSWORD = 'simplePassword'
 NEW_PASSWORD = 'veryMuchStrongerstrPassword'
 OLD_SECRETTEXT = 'This text should be secret\nbut not for your eyes'
 NEW_SECRETTEXT = 'This is what I am going to tell you\nYou are the best'
+OLD_SECRETMAP = {'secret_map_key': 'old_secret_map_value'}
+NEW_SECRETMAP = {'secret_map_key': 'new_secret_map_value'}
 
 # we split secrettext to check if either part of it is in logs, because none should be
-SECRETS = [OLD_PASSWORD, NEW_PASSWORD, *OLD_SECRETTEXT.split('\n'), *NEW_SECRETTEXT.split('\n')]
+SECRETS = [
+    OLD_PASSWORD,
+    NEW_PASSWORD,
+    *OLD_SECRETTEXT.split('\n'),
+    *NEW_SECRETTEXT.split('\n'),
+    OLD_SECRETMAP["secret_map_key"],
+    NEW_SECRETMAP["secret_map_key"],
+]
 
 CHANGE_CONFIG_ACTION = 'change_secrets'
 CHECK_CONFIG_ACTION = 'check'
@@ -44,7 +53,7 @@ def configured_cluster(sdk_client_fs) -> Cluster:
     """Upload bundle, create cluster and set password/secrettext"""
     bundle = sdk_client_fs.upload_from_fs(get_data_dir(__file__, 'cluster'))
     cluster = bundle.cluster_create('Secretly Secret Cluster')
-    _set_cluster_secrets(cluster, OLD_PASSWORD, OLD_SECRETTEXT)
+    _set_cluster_secrets(cluster, OLD_PASSWORD, OLD_SECRETTEXT, OLD_SECRETMAP)
     return cluster
 
 
@@ -54,8 +63,11 @@ def test_secrets_change_via_adcm_config_plugin(adcm_fs, configured_cluster):
     when adcm_config is used
     """
     # run twice to ensure secrets changes doesn't "leak"
-    for password, secrettext in ((NEW_PASSWORD, NEW_SECRETTEXT), (OLD_PASSWORD, OLD_SECRETTEXT)):
-        job_id = _run_change_config(configured_cluster, password, secrettext).job_list()[0].id
+    for password, secrettext, secretmap in (
+        (NEW_PASSWORD, NEW_SECRETTEXT, NEW_SECRETMAP),
+        (OLD_PASSWORD, OLD_SECRETTEXT, OLD_SECRETMAP),
+    ):
+        job_id = _run_change_config(configured_cluster, password, secrettext, secretmap).job_list()[0].id
         _check_no_secrets_in_config(configured_cluster)
         _check_no_secrets_in_job_dir(adcm_fs.container, job_id)
     _check_no_secrets_in_adcm_logs(adcm_fs.container)
@@ -67,7 +79,7 @@ def test_secrets_change_via_config(configured_cluster):
     """
     _check_secrets_in_config_encrypted(configured_cluster)
     _check_no_secrets_in_config(configured_cluster)
-    _set_cluster_secrets(configured_cluster, NEW_PASSWORD, NEW_SECRETTEXT)
+    _set_cluster_secrets(configured_cluster, NEW_PASSWORD, NEW_SECRETTEXT, NEW_SECRETMAP)
     _check_secrets_in_config_encrypted(configured_cluster)
     _check_no_secrets_in_config(configured_cluster)
 
@@ -79,13 +91,13 @@ def test_secrets_are_correct_in_job_when_changed_via_plugin(configured_cluster, 
     """
     configured_cluster.host_add(generic_provider.host_create('some-fqdn'))
     with allure.step('Check secrets set by config change'):
-        _run_check_config(configured_cluster, OLD_PASSWORD, OLD_SECRETTEXT)
+        _run_check_config(configured_cluster, OLD_PASSWORD, OLD_SECRETTEXT, OLD_SECRETMAP)
     with allure.step('Change secrets to new ones with plugin and check'):
-        _run_change_config(configured_cluster, NEW_PASSWORD, NEW_SECRETTEXT)
-        _run_check_config(configured_cluster, NEW_PASSWORD, NEW_SECRETTEXT)
+        _run_change_config(configured_cluster, NEW_PASSWORD, NEW_SECRETTEXT, NEW_SECRETMAP)
+        _run_check_config(configured_cluster, NEW_PASSWORD, NEW_SECRETTEXT, NEW_SECRETMAP)
     with allure.step('Change secrets to old values with plugin and check'):
-        _run_change_config(configured_cluster, OLD_PASSWORD, OLD_SECRETTEXT)
-        _run_check_config(configured_cluster, OLD_PASSWORD, OLD_SECRETTEXT)
+        _run_change_config(configured_cluster, OLD_PASSWORD, OLD_SECRETTEXT, OLD_SECRETMAP)
+        _run_check_config(configured_cluster, OLD_PASSWORD, OLD_SECRETTEXT, OLD_SECRETMAP)
 
 
 # !===== Checks =====!
@@ -112,8 +124,10 @@ def _check_secrets_in_config_encrypted(cluster: Cluster):
     secret_config_fields = (
         config['password'],
         config['secrettext'],
+        config["secretmap"]['secret_map_key'],
         config['group']['password'],
         config['group']['secrettext'],
+        config["group"]["secretmap"]['secret_map_key'],
     )
     assert all(
         field.startswith('$ANSIBLE_VAULT') for field in secret_config_fields
@@ -166,22 +180,22 @@ def _filtered_files(ls_out: str) -> Iterator[str]:
     return filter(lambda x: x != '' and x[-1] != '/', ls_out.split('\n'))
 
 
-def _set_cluster_secrets(cluster: Cluster, password: str, secrettext: str):
-    secrets = {'password': password, 'secrettext': secrettext}
+def _set_cluster_secrets(cluster: Cluster, password: str, secrettext: str, secretmap: dict):
+    secrets = {'password': password, 'secrettext': secrettext, "secretmap": secretmap}
     return cluster.config_set({**secrets, 'group': {**secrets}})
 
 
-def _run_change_config(cluster: Cluster, new_password: str, new_secrettext: str):
+def _run_change_config(cluster: Cluster, new_password: str, new_secrettext: str, new_secretmap: dict):
     return run_cluster_action_and_assert_result(
         cluster,
         CHANGE_CONFIG_ACTION,
-        config={'new_password': new_password, 'new_secrettext': new_secrettext},
+        config={'new_password': new_password, 'new_secrettext': new_secrettext, 'new_secretmap': new_secretmap},
     )
 
 
-def _run_check_config(cluster: Cluster, password: str, secrettext: str):
+def _run_check_config(cluster: Cluster, password: str, secrettext: str, secretmap: dict):
     return run_cluster_action_and_assert_result(
         cluster,
         CHECK_CONFIG_ACTION,
-        config={'password': password, 'secrettext': secrettext},
+        config={'password': password, 'secrettext': secrettext, 'secretmap': secretmap},
     )
