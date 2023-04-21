575de5a74051eae2d09dd9d2dcd62b404ad392b8
Konstantin Shestakov <k.shestakov@arenadata.io>
2023-04-21 13:54:55 +0000

    ADCM-3800 Remove Npmcheck CI job

--- web/npm_check.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -euo pipefail
-
-yarn add npm-check
-export PATH=$PATH:./node_modules/.bin/
-yarn config set registry https://rt.adsw.io/artifactory/api/npm/arenadata-npm/
-yarn install --production
-npm-check --production --skip-unused
-yarn audit
