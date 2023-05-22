26ce86b319169335c027cdd1830edb7c9ddcef4a
Konstantin Shestakov <k.shestakov@arenadata.io>
2023-05-22 11:21:02 +0000

    ADCM-4055 Implement Upgrade endpoints

--- python/api_v2/cluster/urls.py
+++ python/api_v2/cluster/urls.py
@@ -12,6 +12,7 @@
 
 from api_v2.action.views import ActionViewSet
 from api_v2.cluster.views import ClusterViewSet
+from api_v2.upgrade.views import UpgradeViewSet
 from rest_framework_nested.routers import NestedSimpleRouter, SimpleRouter
 
 router = SimpleRouter()
@@ -20,4 +21,7 @@ router.register(prefix="", viewset=ClusterViewSet)
 action_router = NestedSimpleRouter(parent_router=router, parent_prefix="", lookup="cluster")
 action_router.register(prefix="actions", viewset=ActionViewSet)
 
-urlpatterns = [*router.urls, *action_router.urls]
+upgrade_router = NestedSimpleRouter(parent_router=router, parent_prefix="", lookup="cluster")
+upgrade_router.register(prefix="upgrades", viewset=UpgradeViewSet)
+
+urlpatterns = [*router.urls, *action_router.urls, *upgrade_router.urls]
