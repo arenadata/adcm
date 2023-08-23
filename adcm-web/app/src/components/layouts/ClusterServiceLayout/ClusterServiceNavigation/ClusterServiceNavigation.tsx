a2e480ce03a794ad860636c441dcbd2b474c4294
kfedorenko <k.fedorenko@arenadata.io>
2023-08-23 11:11:28 +0300

    ADCM-4390 Fix breadcrumbs on the Cluster page

--- adcm-web/app/src/components/layouts/ClusterServiceLayout/ClusterServiceNavigation/ClusterServiceNavigation.tsx
+++ adcm-web/app/src/components/layouts/ClusterServiceLayout/ClusterServiceNavigation/ClusterServiceNavigation.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Tab, TabsBlock } from '@uikit';
 import SubNavigationWrapper from '@commonComponents/SubNavigationWrapper/SubNavigationWrapper';
+import { useDispatch, useStore } from '@hooks';
+import { setBreadcrumbs } from '@store/adcm/breadcrumbs/breadcrumbsSlice';
+import { useLocation } from 'react-router-dom';
+
+const tabsNavigationDictionary: { [key: string]: string } = {
+  'primary-configuration': 'Primary Configuration',
+  'configuration-groups': 'Configuration Groups',
+  components: 'Components',
+  info: 'Info',
+};
 
 const ClusterServiceNavigation: React.FC = () => {
+  const dispatch = useDispatch();
+  const cluster = useStore(({ adcm }) => adcm.cluster.cluster);
+  const service = useStore(({ adcm }) => adcm.service.service);
+
+  const { pathname } = useLocation();
+  const [, , , , , subTabId] = pathname.split('/');
+
+  useEffect(() => {
+    if (cluster && service) {
+      dispatch(
+        setBreadcrumbs([
+          { href: '/clusters', label: 'Clusters' },
+          { href: `/clusters/${cluster.id}`, label: cluster.name },
+          { href: `/clusters/${cluster.id}/services`, label: 'Services' },
+          { label: service.displayName },
+          { label: tabsNavigationDictionary[subTabId] },
+        ]),
+      );
+    }
+  }, [cluster, service, subTabId, dispatch]);
+
   return (
     <SubNavigationWrapper>
       <TabsBlock variant="secondary">
