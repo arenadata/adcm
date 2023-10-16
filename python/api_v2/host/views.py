64b7c1108102c90415b25a30c08a22caf906f73e
Aleksandr Alferov <aas@arenadata.io>
2023-10-16 12:08:39 +0000

    ADCM-4673 Redesigned host rename method

--- python/api_v2/host/views.py
+++ python/api_v2/host/views.py
@@ -26,10 +26,9 @@ from api_v2.host.utils import (
     maintenance_mode,
     map_list_of_hosts,
 )
-from api_v2.views import CamelCaseReadOnlyModelViewSet
-from cm.api import add_host_to_cluster, delete_host, remove_host_from_cluster
+from api_v2.views import CamelCaseModelViewSet, CamelCaseReadOnlyModelViewSet
+from cm.api import delete_host, remove_host_from_cluster
 from cm.errors import AdcmEx
-from cm.issue import update_hierarchy_issues, update_issue_after_deleting
 from cm.models import Cluster, GroupConfig, Host, HostProvider
 from django_filters.rest_framework.backends import DjangoFilterBackend
 from djangorestframework_camel_case.parser import (
@@ -60,15 +59,12 @@ from adcm.permissions import (
 
 
 # pylint:disable-next=too-many-ancestors
-class HostViewSet(
-    ModelObjectPermissionsByActionMixin, PermissionListMixin, ConfigSchemaMixin, CamelCaseReadOnlyModelViewSet
-):
+class HostViewSet(ModelObjectPermissionsByActionMixin, PermissionListMixin, ConfigSchemaMixin, CamelCaseModelViewSet):
     queryset = (
         Host.objects.select_related("provider", "cluster")
         .prefetch_related("concerns", "hostcomponent_set")
         .order_by("fqdn")
     )
-    serializer_class = HostSerializer
     permission_required = [VIEW_HOST_PERM]
     filterset_class = HostFilter
     filter_backends = (DjangoFilterBackend,)
@@ -81,9 +77,9 @@ class HostViewSet(
         elif self.action == "maintenance_mode":
             return HostChangeMaintenanceModeSerializer
 
-        return self.serializer_class
+        return HostSerializer
 
-    def create(self, request, *args, **kwargs):  # pylint:disable=unused-argument
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
@@ -105,48 +101,32 @@ class HostViewSet(
 
         return Response(data=HostSerializer(instance=host).data, status=HTTP_201_CREATED)
 
-    def destroy(self, request, *args, **kwargs):  # pylint: disable=unused-argument
+    def destroy(self, request, *args, **kwargs):
         host = self.get_object()
         check_custom_perm(request.user, "remove", "host", host)
         delete_host(host=host)
         return Response(status=HTTP_204_NO_CONTENT)
 
-    def _host_update(self, request, *args, partial=False, **kwargs):  # pylint: disable=unused-argument
-        serializer = self.get_serializer(data=request.data)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+
+        instance = self.get_object()
+        check_custom_perm(request.user, "change", "host", instance)
+
+        serializer = self.get_serializer(instance=instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         valid = serializer.validated_data
 
-        host = self.get_object()
-        check_custom_perm(request.user, "change", "host", host)
-
-        if not partial and not all((valid.get("fqdn"), valid.get("cluster"))):
-            raise AdcmEx(code="HOST_UPDATE_ERROR", msg="Not all values presented in full update")
-
-        if valid.get("fqdn") and valid.get("fqdn") != host.fqdn and (host.cluster or host.state != "created"):
+        if (
+            valid.get("fqdn")
+            and valid.get("fqdn") != instance.fqdn
+            and (instance.cluster or instance.state != "created")
+        ):
             raise AdcmEx(code="HOST_UPDATE_ERROR")
 
-        if valid.get("fqdn"):
-            host.fqdn = valid["fqdn"]
+        serializer.save()
 
-        if valid.get("cluster"):
-            check_custom_perm(request.user, "map_host_to", "cluster", valid["cluster"])
-            add_host_to_cluster(cluster=valid["cluster"], host=host)
-        elif valid.get("cluster") is None and host.cluster:
-            check_custom_perm(request.user, "unmap_host_from", "cluster", valid["cluster"])
-            remove_host_from_cluster(host=host)
-        elif valid.get("cluster") is None:
-            host.save()
-            update_hierarchy_issues(host.cluster)
-            update_hierarchy_issues(host.provider)
-            update_issue_after_deleting()
-
-        return Response(status=HTTP_200_OK, data=HostSerializer(host).data)
-
-    def partial_update(self, request, *args, **kwargs):
-        return self._host_update(request, *args, partial=True, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        return self._host_update(request, *args, **kwargs)
+        return Response(status=HTTP_200_OK, data=HostSerializer(instance=instance).data)
 
     @action(methods=["post"], detail=True, url_path="maintenance-mode")
     def maintenance_mode(self, request: Request, *args, **kwargs) -> Response:  # pylint: disable=unused-argument
