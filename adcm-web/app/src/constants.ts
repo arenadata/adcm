06d87f8599b25f35185e80507d76237e7dc97f5d
Vladimir Remizov <v.remizov@arenadata.io>
2023-10-18 10:00:44 +0000

    [ui] bugfix/ADCM-4730 fix prod ws

--- adcm-web/app/src/constants.ts
+++ adcm-web/app/src/constants.ts
@@ -1,7 +1,9 @@
 import { getEnv } from '@utils/envVarsUtils';
 
 export const apiHost = getEnv('ADCM_API_HOST') ?? '';
-export const wsHost = getEnv('ADCM_WS_HOST');
+
+const defaultSocketSchema = window.location.protocol === 'https:' ? 'wss' : 'ws';
+export const wsHost = getEnv('ADCM_WS_HOST') ?? `${defaultSocketSchema}://${window.location.host}`;
 
 export const defaultPerPagesList = [
   { value: 10, label: '10 per page' },
