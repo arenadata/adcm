7fc1ddace62a988c3a8667722f8b4aba838af1b9
dbardin90 <d.bardin@arenadata.io>
2023-10-18 13:15:24 +0300

    ADCM-4728 - [UI 2.0] Impossible to delete Hostprovider

--- adcm-web/app/src/components/pages/HostProvidersPage/Dialogs/index.tsx
+++ adcm-web/app/src/components/pages/HostProvidersPage/Dialogs/index.tsx
@@ -1,11 +1,13 @@
 import CreateHostProviderDialog from './CreateHostProviderDialog/CreateHostProviderDialog';
 import HostProviderDynamicActionDialog from './HostProviderDynamicActionDialog/HostProviderDynamicActionDialog';
 import HostProviderUpgradeDialog from './HostProviderUpgradeDialog/HostProviderUpgradeDialog';
+import HostProvidersDeleteDialog from '@pages/HostProvidersPage/Dialogs/HostProvidersDeleteDialog/HostProvidersDeleteDialog';
 
 const HostProviderDialogs = () => {
   return (
     <>
       <CreateHostProviderDialog />
+      <HostProvidersDeleteDialog />
       <HostProviderDynamicActionDialog />
       <HostProviderUpgradeDialog />
     </>
