7bb387fcc61875d527add3b39906dbef125bbb89
remizov <v.remizov@arenadata.io>
2023-10-22 17:46:49 +0300

    4760 policies table page

--- adcm-web/app/src/components/pages/AccessManagerPage/AccessManagerPoliciesPage/useRequestAccessManagerPolicies.ts
+++ adcm-web/app/src/components/pages/AccessManagerPage/AccessManagerPoliciesPage/useRequestAccessManagerPolicies.ts
@@ -2,14 +2,19 @@ import { useEffect } from 'react';
 import { useStore, useDispatch, useRequestTimer, useDebounce } from '@hooks';
 import { defaultDebounceDelay } from '@constants';
 import { cleanupPolicies, getPolicies, refreshPolicies } from '@store/adcm/policies/policiesSlice';
+import { cleanupList } from '@store/adcm/policies/policiesTableSlice';
+import { usePersistRbacPoliciesTableSettings } from './usePersistRbacPoliciesTableSettings';
 
 export const useRequestAccessManagerPolicies = () => {
   const dispatch = useDispatch();
   const { filter, sortParams, paginationParams } = useStore((s) => s.adcm.policiesTable);
 
+  usePersistRbacPoliciesTableSettings();
+
   useEffect(() => {
     return () => {
       dispatch(cleanupPolicies());
+      dispatch(cleanupList());
     };
   }, [dispatch]);
 
