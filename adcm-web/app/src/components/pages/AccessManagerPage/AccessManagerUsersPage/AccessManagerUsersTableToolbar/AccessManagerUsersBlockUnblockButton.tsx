4c3e3b9572d29e37b8819993088a26cfe3b2ba19
remizov <v.remizov@arenadata.io>
2023-10-22 17:15:13 +0300

    4760 users table page

--- adcm-web/app/src/components/pages/AccessManagerPage/AccessManagerUsersPage/AccessManagerUsersTableToolbar/AccessManagerUsersBlockUnblockButton.tsx
+++ adcm-web/app/src/components/pages/AccessManagerPage/AccessManagerUsersPage/AccessManagerUsersTableToolbar/AccessManagerUsersBlockUnblockButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@uikit';
 import { useDispatch, useStore } from '@hooks';
-import { blockUsers, unblockUsers } from '@store/adcm/users/usersActionsSlice';
+import { unblockUsers } from '@store/adcm/users/usersActionsSlice';
 import { AdcmUserStatus } from '@models/adcm';
 
 const AccessManagerUsersBlockUnblockButtons: React.FC = () => {
@@ -9,16 +9,16 @@ const AccessManagerUsersBlockUnblockButtons: React.FC = () => {
 
   const users = useStore((s) => s.adcm.users.users);
   const selectedItemsIds = useStore(({ adcm }) => adcm.usersActions.selectedItemsIds);
-  const isSelectedSomeActiveUsers = users.some(
-    (user) => selectedItemsIds.includes(user.id) && user.status === AdcmUserStatus.Active,
-  );
+  // const isSelectedSomeActiveUsers = users.some(
+  //   (user) => selectedItemsIds.includes(user.id) && user.status === AdcmUserStatus.Active,
+  // );
   const isSelectedSomeBlockedUsers = users.some(
     (user) => selectedItemsIds.includes(user.id) && user.status === AdcmUserStatus.Blocked,
   );
 
-  const handleBlockClick = () => () => {
-    dispatch(blockUsers(selectedItemsIds));
-  };
+  // const handleBlockClick = () => () => {
+  //   dispatch(blockUsers(selectedItemsIds));
+  // };
 
   const handleUnblockClick = () => () => {
     dispatch(unblockUsers(selectedItemsIds));
@@ -26,9 +26,10 @@ const AccessManagerUsersBlockUnblockButtons: React.FC = () => {
 
   return (
     <>
-      <Button variant="secondary" disabled={!isSelectedSomeActiveUsers} onClick={handleBlockClick()}>
-        Block
-      </Button>
+      {/*If after first release, 2 month, and this is will not need revert then should remove blocked functional */}
+      {/*<Button variant="secondary" disabled={!isSelectedSomeActiveUsers} onClick={handleBlockClick()}>*/}
+      {/*  Block*/}
+      {/*</Button>*/}
       <Button variant="secondary" disabled={!isSelectedSomeBlockedUsers} onClick={handleUnblockClick()}>
         Unblock
       </Button>
