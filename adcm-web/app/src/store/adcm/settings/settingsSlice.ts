486db2a9f9a8ecd0ed4ab106485dd03957bd475e
Kirill Fedorenko <k.fedorenko@arenadata.io>
2023-11-10 10:55:29 +0000

    ADCM-4940 [UI] Add WS handlers for the ADCM object
    https://tracker.yandex.ru/ADCM-4940

--- adcm-web/app/src/store/adcm/settings/settingsSlice.ts
+++ adcm-web/app/src/store/adcm/settings/settingsSlice.ts
@@ -4,6 +4,7 @@ import { AdcmSettings } from '@models/adcm';
 import { createSlice } from '@reduxjs/toolkit';
 import { showError } from '@store/notificationsSlice';
 import { getErrorMessage } from '@utils/httpResponseUtils';
+import { wsActions } from '@store/middlewares/wsMiddleware.constants';
 
 interface AdcmSettingsState {
   adcmSettings: AdcmSettings | null;
@@ -37,6 +38,27 @@ const adcmSettingsSlice = createSlice({
     });
     builder.addCase(getAdcmSettings.rejected, (state) => {
       state.adcmSettings = null;
+    });
+    builder.addCase(wsActions.create_adcm_concern, (state, action) => {
+      const { id: adcmId, changes: newConcern } = action.payload.object;
+      if (
+        state.adcmSettings?.id === adcmId &&
+        state.adcmSettings.concerns.every((concern) => concern.id !== newConcern.id)
+      ) {
+        state.adcmSettings = {
+          ...state.adcmSettings,
+          concerns: [...state.adcmSettings.concerns, newConcern],
+        };
+      }
+    });
+    builder.addCase(wsActions.delete_adcm_concern, (state, action) => {
+      const { id, changes } = action.payload.object;
+      if (state.adcmSettings?.id === id) {
+        state.adcmSettings = {
+          ...state.adcmSettings,
+          concerns: state.adcmSettings.concerns.filter((concern) => concern.id !== changes.id),
+        };
+      }
     });
   },
 });
