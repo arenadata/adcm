2530cbe87c783ad4c3fe411cf5be61e4135ec420
Vladislav Paskov <v.paskov@arenadata.io>
2023-12-11 08:43:00 +0000

    ADCM-5091: [UI 2.0] Local storage of pagination and sorting for Host-components page
    https://tracker.yandex.ru/ADCM-5091

---
+++ adcm-web/app/src/components/pages/cluster/host/HostComponents/usePersistHostComponentsTableSettings.ts
@@ -0,0 +1,46 @@
+import { useDispatch, usePersistSettings, useStore } from '@hooks';
+import { AdcmHostComponentsFilter } from '@models/adcm';
+import { setSortParams, setFilter, setPaginationParams } from '@store/adcm/hostComponents/hostComponentsTableSlice';
+import { mergePaginationParams } from '@hooks/usePersistSettings';
+
+const mergeFilters = (
+  filterFromStorage: AdcmHostComponentsFilter,
+  actualFilter: AdcmHostComponentsFilter,
+): AdcmHostComponentsFilter => {
+  const result: AdcmHostComponentsFilter = {
+    ...actualFilter,
+    ...filterFromStorage,
+    displayName: filterFromStorage.displayName || undefined,
+  };
+
+  return result;
+};
+
+export const usePersistHostComponentsTableSettings = () => {
+  const dispatch = useDispatch();
+
+  const filter = useStore(({ adcm }) => adcm.hostComponentsTable.filter);
+  const sortParams = useStore(({ adcm }) => adcm.hostComponentsTable.sortParams);
+  const paginationParams = useStore(({ adcm }) => adcm.hostComponentsTable.paginationParams);
+
+  const { perPage } = paginationParams;
+
+  usePersistSettings(
+    {
+      localStorageKey: 'adcm/hostComponentsTable',
+      settings: {
+        filter,
+        sortParams,
+        perPage,
+      },
+      isReadyToLoad: true,
+      onSettingsLoaded: (settings) => {
+        const mergedFilter = mergeFilters(settings.filter, filter);
+        dispatch(setFilter(mergedFilter));
+        dispatch(setSortParams(settings.sortParams));
+        dispatch(setPaginationParams(mergePaginationParams(settings.perPage, paginationParams)));
+      },
+    },
+    [filter, sortParams, perPage],
+  );
+};
