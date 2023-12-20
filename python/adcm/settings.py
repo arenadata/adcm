7f147366b30bad7f227b158e9344c66179e5d4e7
Aleksandr Alferov <aas@arenadata.io>
2023-12-20 15:44:12 +0300

    ADCM-5144 Remove adcm-info endpoint

--- python/adcm/settings.py
+++ python/adcm/settings.py
@@ -331,6 +331,7 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "Arenadata Cluster Manager",
     "VERSION": "2.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
+    "SCHEMA_PATH_PREFIX": r"/api/v[0-9]",
     "SWAGGER_UI_DIST": "SIDECAR",
     "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
     "REDOC_DIST": "SIDECAR",
