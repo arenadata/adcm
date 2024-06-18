fd9d75fc508a975b77e1746b1205a7dbf53311fc
Artem Starovoitov <a.starovoitov@arenadata.io>
2024-06-18 07:49:03 +0000

    ADCM-5666: Fix message for group

--- python/ansible_plugin/executors/check.py
+++ python/ansible_plugin/executors/check.py
@@ -42,8 +42,8 @@ class CheckArguments(BaseStrictModel):
     fail_msg: str | None = None
     success_msg: str | None = None
     group_title: str | None = None
-    group_success_msg: str | None = None
-    group_fail_msg: str | None = None
+    group_success_msg: str = ""
+    group_fail_msg: str = ""
 
     @model_validator(mode="after")
     def check_msg_is_specified_if_no_fail_success_msg(self) -> Self:
@@ -60,24 +60,6 @@ class CheckArguments(BaseStrictModel):
 
         if self.success_msg is None or self.fail_msg is None:
             message = "Both success_msg and fail_msg should be specified when msg is absent"
-            raise ValueError(message)
-
-        return self
-
-    @model_validator(mode="after")
-    def check_group_msg_if_group_is_specified_if_no_msg(self) -> Self:
-        if self.msg:
-            return self
-
-        if (
-            self.group_title is not None
-            and self.group_success_msg is None
-            and self.group_title is not None
-            and self.group_fail_msg is None
-        ):
-            message = (
-                "either 'group_fail_msg' or 'group_success_msg' or msg must be specified if 'group_title' is specified"
-            )
             raise ValueError(message)
 
         return self
--- python/ansible_plugin/tests/test_adcm_check.py
+++ python/ansible_plugin/tests/test_adcm_check.py
@@ -336,3 +336,29 @@ class TestCheckPluginExecutor(BaseTestEffectsOfADCMAnsiblePlugins):
         self.assertEqual(GroupCheckLog.objects.all().count(), 0)
         self.assertEqual(CheckLog.objects.all().count(), 0)
         self.assertEqual(LogStorage.objects.all().count(), 2)
+
+    def test_adcm_check_group_msg_cant_be_null_fail(self) -> None:
+        task = self.prepare_task(owner=self.cluster, name="dummy")
+        job, *_ = JobRepoImpl.get_task_jobs(task.id)
+
+        executor = self.prepare_executor(
+            executor_type=ADCMCheckPluginExecutor,
+            call_arguments="""
+                title: title
+                result: true
+                msg: test_message
+                group_title: group
+                group_success_msg: null
+            """,
+            call_context=job,
+        )
+        result = executor.execute()
+
+        self.assertIsInstance(result.error, PluginValidationError)
+        self.assertIn("Arguments doesn't match expected schema", result.error.message)
+        self.assertDictEqual(result.value, {})
+        self.assertFalse(result.changed)
+
+        self.assertEqual(GroupCheckLog.objects.all().count(), 0)
+        self.assertEqual(CheckLog.objects.all().count(), 0)
+        self.assertEqual(LogStorage.objects.all().count(), 2)
