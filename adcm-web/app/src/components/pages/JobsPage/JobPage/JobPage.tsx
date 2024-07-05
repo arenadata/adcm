9964f092a28c388213949f4d21bae18e27139e92
Pavel Nesterovkiy <npp@arenadata.io>
2024-07-05 07:25:16 +0000

    bugfix/ADCM-5752 autoscroll for single jobLog
    https://tracker.yandex.ru/ADCM-5752

--- adcm-web/app/src/components/pages/JobsPage/JobPage/JobPage.tsx
+++ adcm-web/app/src/components/pages/JobsPage/JobPage/JobPage.tsx
@@ -5,8 +5,8 @@ import JobsSubPageStopHeader from './JobPageHeader/JobPageHeader';
 import { useEffect } from 'react';
 import { setBreadcrumbs } from '@store/adcm/breadcrumbs/breadcrumbsSlice';
 import JobPageChildJobsTable from './JobPageChildJobsTable/JobPageChildJobsTable';
-import JobPageLog from './JobPageLog/JobPageLog';
 import JobPageStopJobDialog from './Dialogs/JobPageStopJobDialog';
+import SingleJob from '@pages/JobsPage/JobPage/SingleJob';
 
 const JobPage: React.FC = () => {
   const { task, dispatch } = useRequestJobPage();
@@ -24,7 +24,7 @@ const JobPage: React.FC = () => {
       <TableContainer variant="easy">
         <JobPageTable />
       </TableContainer>
-      {task.childJobs?.length === 1 && <JobPageLog id={task.childJobs[0].id} />}
+      {task.childJobs?.length === 1 && <SingleJob task={task} />}
       {task.childJobs && task.childJobs.length > 1 && (
         <TableContainer variant="easy">
           <JobPageChildJobsTable />
