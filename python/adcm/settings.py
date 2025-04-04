0afdddc07cfcd504b0d9e832cd6ba0ecb6628261
astarovo <a.starovoitov@arenadata.io>
2025-04-04 15:25:53 +0300

    ADCM-6488: Make db_port env optional

--- python/adcm/settings.py
+++ python/adcm/settings.py
@@ -176,7 +176,7 @@ DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT", default="5432")
 
 if all((DB_PASS, DB_NAME, DB_USER, DB_HOST, DB_PORT)):
     DB_DEFAULT = {
