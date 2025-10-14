8485b65e72dbcc06833e46b85a54bc04e63b912d
Kirill Fedorenko <k.fedorenko@arenadata.io>
2025-10-14 14:59:30 +0300

    ADCM-7011 [UI] Add the job object name to the bell drop-down window
    https://tracker.yandex.ru/ADCM-7011

--- adcm-web/app/src/components/common/Table/Cells/JobsStatusCell/JobsStatusCell.tsx
+++ adcm-web/app/src/components/common/Table/Cells/JobsStatusCell/JobsStatusCell.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
 import s from './JobsStatusCell.module.scss';
 import { TableCell } from '@uikit';
-import JobsStatusIconCell from './JobsStatusIcon/JobsStatusIcon';
+import JobsStatusIcon from '@commonComponents/JobsStatusIcon/JobsStatusIcon';
 import type { AdcmJobStatus } from '@models/adcm';
 import cn from 'classnames';
 
@@ -13,7 +13,7 @@ interface JobsStatusCellProps extends React.HTMLAttributes<HTMLDivElement> {
 const JobsStatusCell: React.FC<JobsStatusCellProps> = ({ children, status, className }) => {
   return (
     <TableCell className={cn(s.cell, className)}>
-      <JobsStatusIconCell status={status} />
+      <JobsStatusIcon status={status} />
       {children}
     </TableCell>
   );
