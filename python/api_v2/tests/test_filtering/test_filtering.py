da698fe05b28cbb392ba62d01504c53ad4fa5ea6
Polina Ikonnikova <p.ikonnikova@arenadata.io>
2026-04-20 07:23:46 +0000

    ADCM-7960 Add unit tests to verify filtering and sorting of the hostprovider list

--- python/api_v2/tests/test_filtering/test_filtering.py
+++ python/api_v2/tests/test_filtering/test_filtering.py
@@ -11,147 +11,138 @@
 # limitations under the License.
 
 from cm.legacy.services.status.client import FullStatusMap
-from cm.models import ADCMEntityStatus, SignatureStatus
+from tests.client import APINode
 from tests.dependencies import get_status_scenarios_manager
 from tests.suites import ADCMFiltersDataSuite
 
+from api_v2.tests.test_filtering.cases import (
+    BundlesCase,
+    ClustersCase,
+    HostProvidersCase,
+)
 
-class TestFilters(ADCMFiltersDataSuite):
-    def setUp(self) -> None:
-        super().setUp()
 
-        self.clusters_url = self.client.v2 / "clusters"
-        self.bundles_url = self.client.v2 / "bundles"
-
-        self.set_cluster_state(self.cl_1, "installed")
-        self.set_bundle_signature_status(self.bundle_cl_1, SignatureStatus.VALID)
-
-    def test_ordering_bundles(self) -> None:
-        cases = [
-            ("displayName", "displayName", ["A Cluster", "A Cluster", "B Cluster"]),
-            (
-                "uploadTime",
-                "uploadTime",
-                [
-                    self.normalize_upload_time(value)
-                    for value in (self.bundle_cl_1.date, self.bundle_cl_2.date, self.bundle_cl_3.date)
-                ],
-            ),
-            ("version", "version", ["1.0.1", "12.0.0", "2.0.0"]),
-            ("edition", "edition", ["community", "community", "enterprise"]),
-            (
-                "mainPrototypeLicenseStatus",
-                "mainPrototype.license.status",
-                ["absent", "absent", "accepted"],
-            ),
-            ("signatureStatus", "signatureStatus", ["absent", "absent", "valid"]),
-        ]
-
-        for ordering_field, value_path, asc_expected_value in cases:
+class FiltersBaseCheck(ADCMFiltersDataSuite):
+    def check_ordering(self, *, url: APINode, ordering_cases: list) -> None:
+        for ordering_field, value_path, asc_expected_value in ordering_cases:
             with self.subTest(direction="asc", ordering_field=ordering_field):
-                results = self.get_results(self.bundles_url, value_path, query={"ordering": ordering_field})
+                results = self.get_results(url, value_path, query={"ordering": ordering_field})
                 self.assertEqual(asc_expected_value, results)
 
             with self.subTest(direction="desc", ordering_field=ordering_field):
-                results = self.get_results(self.bundles_url, value_path, query={"ordering": f"-{ordering_field}"})
+                results = self.get_results(url, value_path, query={"ordering": f"-{ordering_field}"})
                 self.assertEqual(list(reversed(asc_expected_value)), results)
 
-    def test_filters_bundles(self) -> None:
-        cases = [
-            ("id", "id", self.bundle_cl_1.pk, [self.bundle_cl_1.pk], "0"),
-            ("displayName", "displayName", "A CLU", ["A Cluster", "A Cluster"], "wrong"),  # check icontains
-            ("edition", "edition", "enterprise", ["enterprise"], "ent"),  # check exact
-            ("version", "version", "1.0.1", ["1.0.1"], "1"),  # check exact
-            ("mainPrototypeLicenseStatus", "mainPrototype.license.status", "accepted", ["accepted"], "unaccepted"),
-            (
-                "product",
-                "mainPrototype.name",
-                "B_CLUSTER",
-                [
-                    "b_cluster",
-                ],
-                "b_clus",
-            ),  # check iexact
-            (
-                "uploadTime",
-                "uploadTime",
-                self.normalize_upload_time(self.bundle_cl_1.date),
-                [self.normalize_upload_time(self.bundle_cl_1.date)],
-                "2020-04-09T12:48:02.075849Z",
-            ),
-            ("signatureStatus", "signatureStatus", "valid", ["valid"], "invalid"),
-        ]
-
-        for filter_field, value_path, matched_query_value, matched_expected_value, empty_query_value in cases:
+    def check_filters(self, *, url: APINode, filters_cases: list) -> None:
+        for (
+            filter_field,
+            value_path,
+            matched_query_value,
+            matched_expected_value,
+            empty_query_value,
+        ) in filters_cases:
             with self.subTest(filter_result="matched", filter_field=filter_field):
-                results = self.get_results(self.bundles_url, value_path, query={filter_field: matched_query_value})
+                results = self.get_results(url, value_path, query={filter_field: matched_query_value})
                 self.assertEqual(matched_expected_value, results)
 
             with self.subTest(filter_result="empty", filter_field=filter_field):
-                results = self.get_results(self.bundles_url, value_path, query={filter_field: empty_query_value})
+                results = self.get_results(url, value_path, query={filter_field: empty_query_value})
                 self.assertEqual([], results)
 
-    def test_ordering_clusters(self) -> None:
-        cases = [
-            ("name", "name", ["cluster_1", "cluster_2", "cluster_3"]),
-            ("prototypeName", "prototype.name", ["a_cluster", "a_cluster", "b_cluster"]),
-            (
-                "prototypeDisplayName",
-                "prototype.displayName",
-                ["A Cluster", "A Cluster", "B Cluster"],
-            ),
-            ("prototypeVersion", "prototype.version", ["1.0.1", "12.0.0", "2.0.0"]),
-            ("state", "state", ["created", "created", "installed"]),
-        ]
+    def check_filter_status(self, *, url: APINode, checked_status: str) -> None:
+        """
+        Check the filter by the status parameter.
 
-        for ordering_field, value_path, asc_expected_value in cases:
-            with self.subTest(direction="asc", ordering_field=ordering_field):
-                results = self.get_results(self.clusters_url, value_path, query={"ordering": ordering_field})
-                self.assertEqual(asc_expected_value, results)
+        This method is implemented separately from the other filters for the following reasons:
+        1. The parameter can only have two values: "up" and "down". In this case,
+            checking for an empty list is not a filtering check.
+        2. All status value settings for tests are located in one place and will
+            only be set for this test.
+        """
 
-            with self.subTest(direction="desc", ordering_field=ordering_field):
-                results = self.get_results(self.clusters_url, value_path, query={"ordering": f"-{ordering_field}"})
-                expected_values = list(reversed(asc_expected_value))
-                self.assertEqual(expected_values, results)
-
-    def test_filters_clusters(self) -> None:
-        cases = [
-            ("id", "id", self.cl_1.pk, [self.cl_1.pk], "0"),
-            ("name", "name", "ER_1", ["cluster_1"], "wrong"),  #  check icontains
-            ("prototypeName", "prototype.name", "a_cluster", ["a_cluster", "a_cluster"], "wrong"),
-            (
-                "prototypeDisplayName",
-                "prototype.displayName",
-                "A Cluster",
-                ["A Cluster", "A Cluster"],
-                "wrong",
-            ),
-            ("state", "state", "installed", ["installed"], "wrong"),
-            ("prototypeVersion", "prototype.version", "1.0.1", ["1.0.1"], "1"),
-        ]
-
-        for filter_field, value_path, matched_query_value, matched_expected_value, empty_query_value in cases:
-            with self.subTest(filter_result="matched", filter_field=filter_field):
-                results = self.get_results(self.clusters_url, value_path, query={filter_field: matched_query_value})
-                self.assertEqual(matched_expected_value, results)
-
-            with self.subTest(filter_result="empty", filter_field=filter_field):
-                results = self.get_results(self.clusters_url, value_path, query={filter_field: empty_query_value})
-                self.assertEqual([], results)
-
-    def test_status_filters(self) -> None:
         status_map = FullStatusMap.model_validate(
             {
                 "clusters": {
-                    str(self.cl_1.pk): {"services": {}, "status": 0, "hosts": {}},
+                    str(self.cl_1.pk): {
+                        "services": {
+                            str(self.service_1.pk): {
+                                "status": 0,
+                                "components": {},
+                                "details": [],
+                            },
+                        },
+                        "status": 0,
+                        "hosts": {},
+                    },
                 }
             }
         )
 
         manager = get_status_scenarios_manager()
         manager.set_status_map(status_map)
-        response = self.get_r(url=self.clusters_url, query={"status": "up"})
 
+        response = self.get_r(url=url, query={"status": checked_status})
         result = self.extract_values(response["results"], "status")
-        expected_value = [ADCMEntityStatus.UP.value]
+
+        expected_value = [checked_status]
         self.assertEqual(expected_value, result)
+
+
+class TestAPIFilters(FiltersBaseCheck):
+    """
+    The class with tests for checking API filters and sorting.
+
+    Each test declares a test case related to the target entity and one of three methods
+    for checks: check_ordering, check_filters, check_filter_status.
+
+    The method name consists: test_{check name}_{entity name}.
+
+    If you want to add a special test, the name should be:
+        test_{check name}_{entity name}_{feature}.
+    """
+
+    def test_filters_clusters(self) -> None:
+        case = ClustersCase(self)
+        self.check_filters(
+            url=case.get_url(),
+            filters_cases=case.get_filters_cases(),
+        )
+
+    def test_ordering_clusters(self) -> None:
+        case = ClustersCase(self)
+        self.check_ordering(
+            url=case.get_url(),
+            ordering_cases=case.get_ordering_cases(),
+        )
+
+    def test_filter_status_clusters(self) -> None:
+        case = ClustersCase(self)
+        self.check_filter_status(url=case.get_url(), checked_status="up")
+
+    def test_filters_bundles(self) -> None:
+        case = BundlesCase(self)
+        self.check_filters(
+            url=case.get_url(),
+            filters_cases=case.get_filters_cases(),
+        )
+
+    def test_ordering_bundles(self) -> None:
+        case = BundlesCase(self)
+        self.check_ordering(
+            url=case.get_url(),
+            ordering_cases=case.get_ordering_cases(),
+        )
+
+    def test_filters_hostproviders(self) -> None:
+        case = HostProvidersCase(self)
+        self.check_filters(
+            url=case.get_url(),
+            filters_cases=case.get_filters_cases(),
+        )
+
+    def test_ordering_hostproviders(self) -> None:
+        case = HostProvidersCase(self)
+        self.check_ordering(
+            url=case.get_url(),
+            ordering_cases=case.get_ordering_cases(),
+        )
